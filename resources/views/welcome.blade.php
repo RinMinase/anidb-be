7f3733b60e08ebcd1ac546c20bca0be347d29b24
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-24 01:47:54 +0800

    ANIDB: removed favicon request

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">
 
 		<title>Laravel</title>
 
