ca36ec41c5ef6c8260cb8ab8cd200fb4a8b31ff4
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-24 01:24:58 +0800

    ANIDB: removed extra http calls in html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="data:;base64,=">
 
 		<title>Laravel</title>
-
-		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
 		<style>
 			html, body {
