1f06d3e078ad7d572c027e0fd7fa1f38f8d082de
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-24 23:35:19 +0800

    ANIDB: modified language to static value

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Rin Minase's Anime Database Back-end API Service">
+
 		<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">
 
-		<title>Laravel</title>
+		<title>AniDB</title>
 
 		<style>
 			html, body {
