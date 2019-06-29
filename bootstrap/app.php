08b6d91c0ba62889d1aa5a0a80f396961cc0f90a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-29 16:19:39 +0800

    ANIDB: modified comment formatting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 	dirname(__DIR__)
 ))->bootstrap();
 
+
 /*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
-*/
+ *--------------------------------------------------------------------------
+ * Create The Application
+ *--------------------------------------------------------------------------
+ *
+ * Here we will load the environment and create the application instance
+ * that serves as the central piece of this framework. We'll use this
+ * application as an "IoC" container and router for this framework.
+ *
+ */
 
 $app = new Laravel\Lumen\Application(
 	dirname(__DIR__)
@@ -25,16 +26,17 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withEloquent();
 
+
 /*
-|--------------------------------------------------------------------------
-| Register Container Bindings
-|--------------------------------------------------------------------------
-|
-| Now we will register a few bindings in the service container. We will
-| register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
-|
-*/
+ *--------------------------------------------------------------------------
+ * Register Container Bindings
+ *--------------------------------------------------------------------------
+ *
+ * Now we will register a few bindings in the service container. We will
+ * register the exception handler and the console kernel. You may add
+ * your own bindings here if you like or you can make another file.
+ *
+ */
 
 $app->singleton(
 	Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -46,16 +48,17 @@ $app->singleton(
 	App\Console\Kernel::class
 );
 
+
 /*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
+ *--------------------------------------------------------------------------
+ * Register Middleware
+ *--------------------------------------------------------------------------
+ *
+ * Next, we will register the middleware with the application. These can
+ * be global middleware that run before and after each request into a
+ * route or middleware that'll be assigned to some specific routes.
+ *
+ */
 
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
@@ -65,31 +68,33 @@ $app->singleton(
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+
 /*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
+ *--------------------------------------------------------------------------
+ * Register Service Providers
+ *--------------------------------------------------------------------------
+ *
+ * Here we will register all of the application's service providers which
+ * are used to bind services into the container. Service providers are
+ * totally optional, so you are not required to uncomment this line.
+ *
+ */
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+
 /*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
+ *--------------------------------------------------------------------------
+ * Load The Application Routes
+ *--------------------------------------------------------------------------
+ *
+ * Next we will include the routes file so that they can all be added to
+ * the application. This will provide all of the URLs the application
+ * can respond to, as well as the controllers that may handle them.
+ *
+ */
 
 $app->router->group([
 	'namespace' => 'App\Http\Controllers',
@@ -97,4 +102,8 @@ $app->router->group([
 	require __DIR__.'/../routes/web.php';
 });
 
+
+/*
+ * Return Application Configurations
+ */
 return $app;
