80b651cd213bf5ffad2baf44117eb839c421a949
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-29 17:59:07 +0800

    ANIDB: added firebase config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -104,6 +104,32 @@ $app->router->group([
 
 
 /*
+ * Register the Firebase DB
+ */
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$creds = '{'
+	. '"type": ' . '"service_account",'
+	. '"project_id": "' . env('FIRE_PROJECT_ID', '') . '",'
+	. '"private_key_id": "' . env('FIRE_PRIVATE_KEY', '') . '",'
+	. '"private_key": "' . env('FIRE_KEY', '') . '",'
+	. '"client_email": "' . env('FIRE_EMAIL', '') . '",'
+	. '"client_id": "' . env('FIRE_CLIENT_ID', '') . '",'
+	. '"auth_uri": "https://accounts.google.com/o/oauth2/auth",'
+	. '"token_uri": "https://oauth2.googleapis.com/token",'
+	. '"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",'
+	. '"client_x509_cert_url": "' . env('FIRE_CERT_URL', '') . '"' .
+'}';
+
+$app->firebase = (new Factory)
+	-> withServiceAccount(ServiceAccount::fromJson($creds))
+	-> withDisabledAutoDiscovery()
+	-> create();
+
+
+/*
  * Return Application Configurations
  */
 return $app;
