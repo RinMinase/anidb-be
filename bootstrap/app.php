8f151641718f9499c36a4762911469662bef3a9e
Rin Minase <rin.black.raison.detre@gmail.com>
2019-06-29 18:19:23 +0800

    ANIDB: improved firebase config readability

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -110,21 +110,23 @@ $app->router->group([
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
-$creds = '{'
-	. '"type": ' . '"service_account",'
-	. '"project_id": "' . env('FIRE_PROJECT_ID', '') . '",'
-	. '"private_key_id": "' . env('FIRE_PRIVATE_KEY', '') . '",'
-	. '"private_key": "' . env('FIRE_KEY', '') . '",'
-	. '"client_email": "' . env('FIRE_EMAIL', '') . '",'
-	. '"client_id": "' . env('FIRE_CLIENT_ID', '') . '",'
-	. '"auth_uri": "https://accounts.google.com/o/oauth2/auth",'
-	. '"token_uri": "https://oauth2.googleapis.com/token",'
-	. '"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",'
-	. '"client_x509_cert_url": "' . env('FIRE_CERT_URL', '') . '"' .
-'}';
+$creds = json_encode([
+	"type" => "service_account",
+	"project_id" => env('FIRE_PROJECT_ID', ''),
+	"private_key_id" => env('FIRE_PRIVATE_KEY', ''),
+	"private_key" => env('FIRE_KEY', ''),
+	"client_email" => env('FIRE_EMAIL', ''),
+	"client_id" => env('FIRE_CLIENT_ID', ''),
+	"auth_uri" => "https://accounts.google.com/o/oauth2/auth",
+	"token_uri" => "https://oauth2.googleapis.com/token",
+	"auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+	"client_x509_cert_url" => env('FIRE_CERT_URL', ''),
+]);
+
+$validatedCreds = str_replace('\\\\n', '\\n', $creds);
 
 $app->firebase = (new Factory)
-	-> withServiceAccount(ServiceAccount::fromJson($creds))
+	-> withServiceAccount(ServiceAccount::fromJson($validatedCreds))
 	-> withDisabledAutoDiscovery()
 	-> create();
 
