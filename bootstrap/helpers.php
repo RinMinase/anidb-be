ae53d38b4ee51b8baa7dc3ef808acf31ed937b07
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-06 23:49:37 +0800

    ANIDB: added function to display index page in helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,12 +1,43 @@
 <?php
 
 if (! function_exists('mongo_json')) {
-	function mongo_json($query_result)
-	{
+	function mongo_json($query_result) {
 		foreach($query_result as $k => $row) {
 			$data[] = $row;
 		}
 
 		return json_encode($data);
+	}
+}
+
+if (! function_exists('display_index_page')) {
+	function display_index_page($version) {
+		return '<html lang="en"><head>'
+				. '<meta name="viewport" content="width=device-width, initial-scale=1">'
+				. '<link href="https://fonts.googleapis.com/css?family=Nunito:200" rel="stylesheet" type="text/css">'
+				. '<title>Rin\'s AniDB API</title>'
+				. '<style>'
+					. 'body {'
+						. 'margin: 0;'
+						. 'color: #636b6f;'
+						. 'font-family: "Nunito", sans-serif;'
+						. 'height: 100vh;'
+						. 'display: flex;'
+						. 'align-items: center;'
+						. 'justify-content: center;'
+					. '}'
+					. '.title {text-align: center; font-size: 84px; margin: 0 8px;}'
+					. '@media (max-width: 576px) { .title { font-size: 56px; } }'
+					. '.subtitle {'
+						. 'font-size: 18px;'
+						. 'margin-bottom: 30px;'
+						. 'position: fixed;'
+						. 'bottom: 0;'
+					. '}'
+				. '</style>'
+			. '</head><body>'
+				. '<div class="title">Rin\'s AniDB API / Middleware</div>'
+				. '<div class="subtitle">Lumen Framework v' . $version . '</div>'
+			. '</body></html>';
 	}
 }
