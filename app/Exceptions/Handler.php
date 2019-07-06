d3a5bdc4fee8cd2235f32d910b4dd2d9278e9308
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-06 20:11:21 +0800

    ANIDB: removed unnecessary comments

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -11,11 +11,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
 	protected $dontReport = [
 		AuthorizationException::class,
 		HttpException::class,
@@ -23,26 +18,11 @@ class Handler extends ExceptionHandler
 		ValidationException::class,
 	];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $exception
-	 * @return void
-	 */
 	public function report(Exception $exception)
 	{
 		parent::report($exception);
 	}
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $exception
-	 * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
-	 */
 	public function render($request, Exception $exception)
 	{
 		return parent::render($request, $exception);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,15 +7,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 ))->bootstrap();
 
 
-/*
- *--------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
  * Create The Application
  *--------------------------------------------------------------------------
- *
- * Here we will load the environment and create the application instance
- * that serves as the central piece of this framework. We'll use this
- * application as an 'IoC' container and router for this framework.
- *
  */
 
 $app = new Laravel\Lumen\Application(
@@ -27,16 +21,10 @@ $app = new Laravel\Lumen\Application(
 // $app->withEloquent();
 
 
-/*
- *--------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
  * Register Container Bindings
  *--------------------------------------------------------------------------
- *
- * Now we will register a few bindings in the service container. We will
- * register the exception handler and the console kernel. You may add
- * your own bindings here if you like or you can make another file.
- *
- */
+*/
 
 $app->singleton(
 	Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -49,51 +37,19 @@ $app->singleton(
 );
 
 
-/*
- *--------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
  * Register Middleware
  *--------------------------------------------------------------------------
- *
- * Next, we will register the middleware with the application. These can
- * be global middleware that run before and after each request into a
- * route or middleware that'll be assigned to some specific routes.
- *
  */
-
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
 
-/*
- *--------------------------------------------------------------------------
- * Register Service Providers
- *--------------------------------------------------------------------------
- *
- * Here we will register all of the application's service providers which
- * are used to bind services into the container. Service providers are
- * totally optional, so you are not required to uncomment this line.
- *
- */
-
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
-
-
-/*
- *--------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
  * Load The Application Routes
  *--------------------------------------------------------------------------
- *
- * Next we will include the routes file so that they can all be added to
- * the application. This will provide all of the URLs the application
- * can respond to, as well as the controllers that may handle them.
- *
  */
 
 $app->router->group([
