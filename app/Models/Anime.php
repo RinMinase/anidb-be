24798ca3d44eacbb8e22fa9466cb6b32e6b3ff42
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-14 04:01:20 +0800

    ANIDB: added parsing of synonyms, episodes and season

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -6,12 +6,19 @@ class Anime {
 
 	private $url;
 	private $title;
+	private $synonyms;
+	private $episodes;
+	private $premiered;
 
 	public static function parse($input): Anime {
 		$instance = new self();
 
 		$instance->url = $input->filterXPath('//meta[@property=\'og:url\']')->attr('content');
 		$instance->title = $input->filterXPath('//meta[@property=\'og:title\']')->attr('content');
+
+		$instance->synonyms = $instance->parseSynonyms($input);
+		$instance->episodes = $instance->parseEpisodes($input);
+		$instance->premiered = $instance->parsePremiered($input);
 
 		return $instance;
 	}
@@ -20,6 +27,9 @@ class Anime {
 		return [
 			'url' => $this->url,
 			'title' => $this->title,
+			'synonyms' => $this->synonyms,
+			'episodes' => $this->episodes,
+			'premiered' => $this->premiered,
 		];
 	}
 
@@ -29,6 +39,47 @@ class Anime {
 
 	public function getTitle(): string {
 		return $this->title;
+	}
+
+	public function getSynonyms(): string {
+		return $this->synonyms;
+	}
+
+	public function getEpisodes(): int {
+		return $this->episodes;
+	}
+
+	public function getPremiered(): string {
+		return $this->premiered;
+	}
+
+	private function parseSynonyms($input): string {
+		$title = $input->filterXPath('//span[text()="Synonyms:"]');
+		if (!$title->count()) { return null; }
+
+		$titles = trim(str_replace($title->text(), '', $title->parents()->text()));
+
+		return $titles;
+	}
+
+	private function parseEpisodes($input): int {
+		$episodes = $input->filterXPath('//span[text()="Episodes:"]');
+		if (!$episodes->count()) { return null; }
+
+		$episodes = trim(str_replace($episodes->text(), '', $episodes->parents()->text()));
+		if ($episodes === 'Unknown') { return null; }
+
+		return (int)$episodes;
+	}
+
+	private function parsePremiered($input): string {
+		$premiered = $input->filterXPath('//span[text()="Premiered:"]');
+		if (!$premiered->count()) { return null; }
+
+		$premiered = trim(str_replace($premiered->text(), '', $premiered->parents()->text()));
+		if ($premiered === '?') { return null; }
+
+		return $premiered;
 	}
 
 }
