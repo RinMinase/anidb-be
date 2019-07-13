54db226e92a17e83d9fb47c6cb687bdda3a5e9c1
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-14 03:32:29 +0800

    ANIDB: used created model

--- app/Middleware/MAL.php
+++ app/Middleware/MAL.php
@@ -2,10 +2,12 @@
 
 namespace App\Middleware;
 
+use App\Models\Anime;
+
 class MAL {
 
 	public function anime($id) {
-		return app('mal')->request('get', '/anime/' . $id);
+		return Anime::parse(app('mal')->request('get', '/anime/' . $id));
 	}
 
 }
