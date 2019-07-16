47fa49b08b563056c0bb2b98524f5382a1e0dd43
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-17 01:08:56 +0800

    ANIDB: added throwing of exception on timeout

--- app/Middleware/MAL.php
+++ app/Middleware/MAL.php
@@ -2,17 +2,22 @@
 
 namespace App\Middleware;
 
+use Exception;
 use App\Models\Anime;
 use App\Models\AnimeSearch;
 
 class MAL {
 
 	public function anime($id) {
-		return Anime::parse(app('goutte')->request('get', '/anime/' . $id));
+		try {
+			return Anime::parse(app('goutte')->request('get', '/anime/' . $id));
+		} catch (Exception $e) { throw new Exception('Issues in connecting to MAL Servers'); }
 	}
 
 	public function search($query) {
-		return AnimeSearch::parse(app('goutte')->request('get', '/anime.php?q=' . urlencode($query)));
+		try {
+			return AnimeSearch::parse(app('goutte')->request('get', '/anime.php?q=' . urlencode($query)));
+		} catch (Exception $e) { throw new Exception('Issues in connecting to MAL Servers'); }
 	}
 
 }
