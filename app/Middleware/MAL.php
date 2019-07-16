e6fb9f5f4e2bd38f30fbffd9d06eeb3af37b3763
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-17 00:35:25 +0800

    ANIDB: added search function in middleware

--- app/Middleware/MAL.php
+++ app/Middleware/MAL.php
@@ -3,11 +3,16 @@
 namespace App\Middleware;
 
 use App\Models\Anime;
+use App\Models\AnimeSearch;
 
 class MAL {
 
 	public function anime($id) {
 		return Anime::parse(app('goutte')->request('get', '/anime/' . $id));
+	}
+
+	public function search($query) {
+		return AnimeSearch::parse(app('goutte')->request('get', '/anime.php?q=' . urlencode($query)));
 	}
 
 }
