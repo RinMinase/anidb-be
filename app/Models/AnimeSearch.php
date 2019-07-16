4131c382c22bcce8ceab08bb4a0c79cd9c789bd3
Rin Minase <rin.black.raison.detre@gmail.com>
2019-07-17 01:12:08 +0800

    ANIDB: optimized code for readability

--- app/Models/AnimeSearch.php
+++ app/Models/AnimeSearch.php
@@ -20,23 +20,20 @@ class AnimeSearch {
 	}
 
 	private function parseResults($input) {
-		$results = $input->filterXPath('//div[contains(@class, "js-categories-seasonal")]/table/tr[1]');
+		$results = $input
+			->filterXPath('//div[contains(@class, "js-categories-seasonal")]/table/tr[1]')
+			->nextAll()
+			->each(
+				function (Crawler $c) {
+					$url = $c->filterXPath('//td[2]/a')->attr('href');
+					$urlId = explode('/', $url)[4]; // URL retrieved is http://domain.com/route/<id>/sub-route
 
-		$results = $results->nextAll()->each(
-			function (Crawler $c) {
-				$url = $c->filterXPath('//td[2]/a')->attr('href');
-
-				/**
-				 * URL retrieved is http://domain.com/route/<id>/sub-route
-				 */
-				$urlId = explode('/', $url)[4];
-
-				return [
-					'id' => $urlId,
-					'title' => $c->filterXPath('//td[2]/a/strong')->text(),
-				];
-			}
-		);
+					return [
+						'id' => $urlId,
+						'title' => $c->filterXPath('//td[2]/a/strong')->text(),
+					];
+				}
+			);
 
 		return (count($results)) ? array_slice($results, 0, 5) : [];
 	}
