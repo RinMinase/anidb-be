c4e0dddc99fc17cb4f219a82bf5eac72c6ccf7c8
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-12 19:55:28 +0800

    ANIDB: removed temporary function

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,25 +4,12 @@ namespace App\Controllers;
 
 class HomeController {
 
-	public function index() {
-		$data = [
-			'status' => '200',
-			'details' => [
-				'id' => 1,
-				'email' => 'test@email.com',
-				'mobile' => '123000123',
-				'message' => 'This is a test response'
-			]
-		];
-
-		return response()->json($data);
-	}
-
 	// public function query() {
 	// 	$data = app('firebase')->getDatabase()->getReference('hdd')->getValue();
 
 	// 	return response()->json($data);
 	// }
+
 	public function mongo() {
 		$data = app('mongo')->hdd->find();
 
