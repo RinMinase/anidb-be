88d3737c16b4a1ed01105c212ead468c1aaa6399
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-13 00:52:56 +0800

    ANIDB: removed temporary release functions

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,33 +4,6 @@ namespace App\Controllers;
 
 class HomeController {
 
-	public function changelog($limit = 20) {
-		$data = app('release')
-			->get('commits', [
-				'query' => [
-					'per_page' => $limit,
-				],
-			])
-			->getBody()
-			->getContents();
-
-		return response($data)->header('Content-Type', 'application/json');
-	}
-
-	public function issues($limit = 100) {
-		$data = app('release')
-			->get('issues', [
-				'query' => [
-					'per_page' => $limit,
-					'page' => 1,
-				],
-			])
-			->getBody()
-			->getContents();
-
-		return response($data)->header('Content-Type', 'application/json');
-	}
-
 	public function mongo() {
 		$data = app('mongo')->hdd->find();
 
