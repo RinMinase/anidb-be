b0cfc001c6c8256971e39ff8e0e3cbb9abf09a4a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-13 01:57:16 +0800

    RELEASE: improve code readability

--- app/Controllers/ReleaseController.php
+++ app/Controllers/ReleaseController.php
@@ -39,33 +39,18 @@ class ReleaseController {
 
 	private function parseChangelog($data) {
 		$changelog = json_decode($data);
-
 		$data = [];
 
 		foreach ($changelog as $change) {
 			$rawDate = new DateTime($change->commit->author->date);
-			$rawMessage = explode(':', $change->commit->message);
-
-			if (count($rawMessage) == 1) {
-				$rawMessage = preg_split("/ (.+)/", $change->commit->message);
-			}
-
-			if (strpos($rawMessage[1], ', resolved #') !== false) {
-				$rawMessage[1] = str_replace(', resolved #', '', $rawMessage[1]);
-			}
-
-			$rawModule = ltrim(strtolower($rawMessage[0]));
-			$rawModule = str_replace('_', ' ', $rawModule);
-
-			$module = ($rawModule === 'anidb' || $rawModule === 'transition') ? '' : $rawModule;
-			$message = ltrim($rawMessage[1]);
+			$parsedMessage = $this->parseChangelogMessage($change->commit->message);
 
 			$data[] = [
 				'date' => $rawDate->format('M d, Y H:m'),
 				'email' => $change->commit->author->email,
 				'name' => $change->commit->author->name,
-				'message' => $message,
-				'module' => $module,
+				'message' => $parsedMessage->message,
+				'module' => $parsedMessage->module,
 				'url' => $change->url,
 			];
 		}
@@ -75,6 +60,29 @@ class ReleaseController {
 
 	private function parseIssues($data) {
 		return $data;
+	}
+
+	private function parseChangelogMessage($message) {
+		$rawMessage = explode(':', $message);
+
+		if (count($rawMessage) == 1) {
+			$rawMessage = preg_split("/ (.+)/", $message);
+		}
+
+		if (strpos($rawMessage[1], ', resolved #') !== false) {
+			$rawMessage[1] = str_replace(', resolved #', '', $rawMessage[1]);
+		}
+
+		$rawModule = ltrim(strtolower($rawMessage[0]));
+		$rawModule = str_replace('_', ' ', $rawModule);
+
+		$module = ($rawModule === 'anidb' || $rawModule === 'transition') ? '' : $rawModule;
+		$message = ltrim($rawMessage[1]);
+
+		return (object) [
+			'module' => $module,
+			'message' => $message,
+		];
 	}
 
 }
