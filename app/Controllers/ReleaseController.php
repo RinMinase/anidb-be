e6ea403e2381592f371ca3717326ca62db0eebd9
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-13 01:50:45 +0800

    RELEASE: added processing of changelog data

--- app/Controllers/ReleaseController.php
+++ app/Controllers/ReleaseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use DateTime;
+
 class ReleaseController {
 
 	public function changelog($limit = 20) {
@@ -36,7 +38,39 @@ class ReleaseController {
 	}
 
 	private function parseChangelog($data) {
-		return $data;
+		$changelog = json_decode($data);
+
+		$data = [];
+
+		foreach ($changelog as $change) {
+			$rawDate = new DateTime($change->commit->author->date);
+			$rawMessage = explode(':', $change->commit->message);
+
+			if (count($rawMessage) == 1) {
+				$rawMessage = preg_split("/ (.+)/", $change->commit->message);
+			}
+
+			if (strpos($rawMessage[1], ', resolved #') !== false) {
+				$rawMessage[1] = str_replace(', resolved #', '', $rawMessage[1]);
+			}
+
+			$rawModule = ltrim(strtolower($rawMessage[0]));
+			$rawModule = str_replace('_', ' ', $rawModule);
+
+			$module = ($rawModule === 'anidb' || $rawModule === 'transition') ? '' : $rawModule;
+			$message = ltrim($rawMessage[1]);
+
+			$data[] = [
+				'date' => $rawDate->format('M d, Y H:m'),
+				'email' => $change->commit->author->email,
+				'name' => $change->commit->author->name,
+				'message' => $message,
+				'module' => $module,
+				'url' => $change->url,
+			];
+		}
+
+		return json_encode($data);
 	}
 
 	private function parseIssues($data) {
