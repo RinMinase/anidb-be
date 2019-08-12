d738b8f29d92de55eef899c452ba9cddd716dcfa
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-13 00:47:24 +0800

    ANIDB: added temporary function for changelog

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,6 +4,19 @@ namespace App\Controllers;
 
 class HomeController {
 
+	public function changelog($limit = 20) {
+		$data = app('release')
+			->get('commits', [
+				'query' => [
+					'per_page' => $limit,
+				],
+			])
+			->getBody()
+			->getContents();
+
+		return response($data)->header('Content-Type', 'application/json');
+	}
+
 	public function mongo() {
 		$data = app('mongo')->hdd->find();
 
