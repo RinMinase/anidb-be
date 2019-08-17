f8e6c0118f900477a7ba7005a3ced0690c3a445e
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-17 21:16:21 +0800

    SUMMER: added create function

--- app/Controllers/SummerController.php
+++ app/Controllers/SummerController.php
@@ -2,9 +2,27 @@
 
 namespace App\Controllers;
 
+use Illuminate\Http\Request;
 use MongoDB\BSON\ObjectID as MongoID;
 
 class SummerController {
+
+	public function create(Request $request) {
+		if ($request->input('timeStart')
+			&& $request->input('timeEnd')
+			&& $request->input('title')) {
+
+			$query = app('mongo')->summer->insertOne([
+				'timeStart' => $request->input('timeStart'),
+				'timeEnd' => $request->input('timeEnd'),
+				'title' => $request->input('title'),
+			]);
+
+			return response('Success');
+		} else {
+			return response('"timeStart", "timeEnd" and "title" fields are required')->setStatusCode(400);
+		}
+	}
 
 	public function retrieve($params = null) {
 		if (is_null($params)) {
