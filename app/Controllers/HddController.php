90867e8d43cd5e56bb6ebc217a85ba3d743718ca
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-18 13:07:41 +0800

    HDD: added authentication

--- app/Controllers/HddController.php
+++ app/Controllers/HddController.php
@@ -2,20 +2,23 @@
 
 namespace App\Controllers;
 
+use Illuminate\Http\Request;
 use MongoDB\BSON\ObjectID as MongoID;
 
 class HddController {
 
-	public function retrieve($params = null) {
-		if (is_null($params)) {
-			$data = app('mongo')->hdd->find();
-		} else {
-			$data = app('mongo')->hdd->find([
-				'_id' => new MongoID($params),
-			]);
+	public function retrieve($params = null, Request $request) {
+		if (is_authenticated($request)) {
+			if (is_null($params)) {
+				$data = app('mongo')->hdd->find();
+			} else {
+				$data = app('mongo')->hdd->find([ '_id' => new MongoID($params) ]);
+			}
+
+			return response(mongo_json($data))->header('Content-Type', 'application/json');
 		}
 
-		return response(mongo_json($data))->header('Content-Type', 'application/json');
+		return response('API Key Required')->setStatusCode(401);
 	}
 
 }
