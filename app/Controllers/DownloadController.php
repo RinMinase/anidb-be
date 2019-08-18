fef4bb1489efe6aa9e4c3a7dcad76be20f085107
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-18 13:07:33 +0800

    DOWNLOAD: added authentication

--- app/Controllers/DownloadController.php
+++ app/Controllers/DownloadController.php
@@ -2,20 +2,25 @@
 
 namespace App\Controllers;
 
+use Illuminate\Http\Request;
 use MongoDB\BSON\ObjectID as MongoID;
 
 class DownloadController {
 
-	public function retrieve($params = null) {
-		if (is_null($params)) {
-			$data = app('mongo')->download->find();
-		} else {
-			$data = app('mongo')->download->find([
-				'_id' => new MongoID($params),
-			]);
+	public function retrieve($params = null, Request $request) {
+		if (is_authenticated($request)) {
+			if (is_null($params)) {
+				$data = app('mongo')->download->find();
+			} else {
+				$data = app('mongo')->download->find([
+					'_id' => new MongoID($params),
+				]);
+			}
+
+			return response(mongo_json($data))->header('Content-Type', 'application/json');
 		}
 
-		return response(mongo_json($data))->header('Content-Type', 'application/json');
+		return response('API Key Required')->setStatusCode(401);
 	}
 
 }
