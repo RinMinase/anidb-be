59324a20e3036fe6f14c9dbef370ba4715826642
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-18 14:04:52 +0800

    ANIDB: added temporary function for export

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,27 @@
 
 namespace App\Controllers;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class HomeController {
 
 	public function mongo() {
 		$data = app('mongo')->hdd->find();
 
 		return response(mongo_json($data))->header('Content-Type', 'application/json');
+	}
+
+	public function export() {
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		$sheet->setCellValue('A1', 'Test data');
+
+		$writer = new Xlsx($spreadsheet);
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment; filename="test.xlsx"');
+		$writer->save('php://output');
+
+		return $writer;
 	}
 }
