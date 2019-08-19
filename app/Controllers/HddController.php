5469b42627df46598013f245cc20502faae880d6
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-20 00:00:16 +0800

    HDD: removed old auth checker

--- app/Controllers/HddController.php
+++ app/Controllers/HddController.php
@@ -8,17 +8,13 @@ use MongoDB\BSON\ObjectID as MongoID;
 class HddController {
 
 	public function retrieve($params = null, Request $request) {
-		if (is_authenticated($request)) {
-			if (is_null($params)) {
-				$data = app('mongo')->hdd->find();
-			} else {
-				$data = app('mongo')->hdd->find([ '_id' => new MongoID($params) ]);
-			}
-
-			return response(mongo_json($data))->header('Content-Type', 'application/json');
+		if (is_null($params)) {
+			$data = app('mongo')->hdd->find();
+		} else {
+			$data = app('mongo')->hdd->find([ '_id' => new MongoID($params) ]);
 		}
 
-		return response('API Key Required')->setStatusCode(401);
+		return response(mongo_json($data))->header('Content-Type', 'application/json');
 	}
 
 }
