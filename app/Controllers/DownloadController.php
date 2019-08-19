df3933f7f97872a30400cff0c1b54fa61883f36f
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-20 00:00:07 +0800

    DOWNLOAD: removed old auth checker

--- app/Controllers/DownloadController.php
+++ app/Controllers/DownloadController.php
@@ -8,19 +8,15 @@ use MongoDB\BSON\ObjectID as MongoID;
 class DownloadController {
 
 	public function retrieve($params = null, Request $request) {
-		if (is_authenticated($request)) {
-			if (is_null($params)) {
-				$data = app('mongo')->download->find();
-			} else {
-				$data = app('mongo')->download->find([
-					'_id' => new MongoID($params),
-				]);
-			}
-
-			return response(mongo_json($data))->header('Content-Type', 'application/json');
+		if (is_null($params)) {
+			$data = app('mongo')->download->find();
+		} else {
+			$data = app('mongo')->download->find([
+				'_id' => new MongoID($params),
+			]);
 		}
 
-		return response('API Key Required')->setStatusCode(401);
+		return response(mongo_json($data))->header('Content-Type', 'application/json');
 	}
 
 }
