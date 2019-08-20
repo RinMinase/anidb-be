b87ff805fdbfa582d4d31e2565ea12b5cb0770fe
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-21 01:07:43 +0800

    ANIDB: functional api key generation

--- app/Commands/GenerateApiKey.php
+++ app/Commands/GenerateApiKey.php
@@ -7,14 +7,32 @@ use Illuminate\Console\Command;
 
 class GenerateApiKey extends Command {
 
-	protected $signature = "generate:api";
+	protected $signature = "api:generate {length?}";
 	protected $description = "Generate a new API Key";
 
 	public function handle() {
+		$length = ($this->argument('length')) ? $this->argument('length') : 32;
+		$envFile = base_path('.env');
+
 		try {
-			$this->info($this->generateRandomString());
+			if (file_exists($envFile)) {
+				$apiKey = $this->generateRandomString($length);
+
+				$this->info('Client API Key:');
+				$this->info($apiKey);
+
+				file_put_contents(
+					$envFile,
+					preg_replace('/API_KEY=\w*/', 'API_KEY=' . $apiKey, file_get_contents($envFile))
+				);
+
+				$this->info('');
+				$this->info('Placed on ENV File successfully');
+			} else {
+				$this->error('ENV File not present');
+			}
 		} catch (Exception $e) {
-			$this->error("An error occurred");
+			$this->error('An error occurred');
 		}
 	}
 
