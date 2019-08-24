5f634e82284f09c8d5ec4b1b85ae9e95cb949efa
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-25 01:26:52 +0800

    ANIDB: added create hdd to routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -22,6 +22,7 @@ $router->group(
 		$router->get('download[/{params}]', ['uses' => 'DownloadController@retrieve']);
 
 		$router->get('hdd[/{params}]', ['uses' => 'HddController@retrieve']);
+		$router->post('hdd[/{params}]', ['uses' => 'HddController@create']);
 		$router->patch('hdd[/{params}]', ['uses' => 'HddController@update']);
 		$router->delete('hdd[/{params}]', ['uses' => 'HddController@remove']);
 
