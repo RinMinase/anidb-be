927ea6f7d32e671722068ade9918bc027da2f82a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-31 01:54:02 +0800

    USER: added functions for code readability

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController {
 		$user = app('mongo')->users->findOne(['username' => $request->input('username')]);
 
 		if (!isset($user)) {
-			return response('"username" does not exist')->setStatusCode(403);
+			return response('"username" or "password" is invalid')->setStatusCode(403);
 		}
 
 		$isVerified = password_verify($request->input('password'), $user->password);
@@ -29,37 +29,9 @@ class UserController {
 			return response('"username" or "password" is invalid')->setStatusCode(403);
 		}
 
-		$userLoggedIn = app('mongo')->session->findOne(
-			[ 'username' => $user->username ],
-			[ 'sort' => [ 'timeout' => -1 ] ],
-		);
+		$this->checkIfUserIsLoggedIn($user);
 
-		if (isset($userLoggedIn)) {
-			$isLoggedIn = (new DateTime())->getTimestamp() <= $userLoggedIn->timeout;
-
-			if ($isLoggedIn) {
-				return response()->json([
-					'username' => $userLoggedIn->username,
-					'role' => $userLoggedIn->role,
-					'token' => $userLoggedIn->token,
-					'timeout' => $userLoggedIn->timeout,
-				]);
-			}
-		}
-
-		if ($user->role === 3 || $user->role === 2) {
-			$timeout = (new DateTime())->add(new DateInterval('PT3H'))->getTimestamp();
-		} else if ($user->role === 1) {
-			$timeout = (new DateTime())->add(new DateInterval('P7D'))->getTimestamp();
-		}
-
-		$data = [
-			'username' => $user->username,
-			'role' => $user->role,
-			'token' => random_string(64),
-			'timeout' => $timeout,
-		];
-
+		$data = $this->generateLoginToken($user);
 		app('mongo')->session->insertOne($data);
 
 		return response()->json($data);
@@ -88,6 +60,41 @@ class UserController {
 		]);
 
 		return response('Success');
+	}
+
+	private function checkIfUserIsLoggedIn($user) {
+		$userLoggedIn = app('mongo')->session->findOne(
+			[ 'username' => $user->username ],
+			[ 'sort' => [ 'timeout' => -1 ] ],
+		);
+
+		if (isset($userLoggedIn)) {
+			$isLoggedIn = (new DateTime())->getTimestamp() <= $userLoggedIn->timeout;
+
+			if ($isLoggedIn) {
+				return response()->json([
+					'username' => $userLoggedIn->username,
+					'role' => $userLoggedIn->role,
+					'token' => $userLoggedIn->token,
+					'timeout' => $userLoggedIn->timeout,
+				]);
+			}
+		}
+	}
+
+	private function generateLoginToken($user) {
+		if ($user->role === 3 || $user->role === 2) {
+			$timeout = (new DateTime())->add(new DateInterval('PT3H'))->getTimestamp();
+		} else if ($user->role === 1) {
+			$timeout = (new DateTime())->add(new DateInterval('P7D'))->getTimestamp();
+		}
+
+		return [
+			'username' => $user->username,
+			'role' => $user->role,
+			'token' => random_string(64),
+			'timeout' => $timeout,
+		];
 	}
 
 }
