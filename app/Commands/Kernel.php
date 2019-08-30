dda76c2cc18abb02cf819e1ad9fc16b0a284f3d1
Rin Minase <rin.black.raison.detre@gmail.com>
2019-08-31 01:16:34 +0800

    ANIDB: removed unused default artisan commands

--- app/Commands/Kernel.php
+++ app/Commands/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Commands;
 
+use App\Commands\KernelApplication as Artisan;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel {
@@ -9,5 +10,14 @@ class Kernel extends ConsoleKernel {
 	protected $commands = [
 		GenerateApiKey::class,
 	];
+
+	protected function getArtisan() {
+		if (is_null($this->artisan)) {
+			$artisan = new Artisan($this->app, $this->app->make('events'), $this->app->version());
+			$this->artisan = $artisan->resolveCommands($this->commands);
+		}
+
+		return $this->artisan;
+	}
 
 }
---
+++ app/Commands/KernelApplication.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Application as Console;
+
+class KernelApplication extends Console {
+	protected function bootstrap() { }
+}
