cd15047eb37447c8f54fc6e827499a1d7fff0de6
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-01 07:37:30 +0800

    DOCS_IMAGE: added documentation

---
+++ docs/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @api {get} /img/:param Retrieve Image URL
+ * @apiName retrieve
+ * @apiGroup Image
+ *
+ * @apiParam {Path} param Complete image file path (i.e. 'assets/test.jpg')
+ *
+ * @apiSuccess {String} url Signed URL of image
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     https://storage.googleapis.com/example.appspot.com/assets/test.jpg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=example%40appspot.gserviceaccount.com{{url and key contents}}
+ *
+ * @apiError NoSuchKey The specified key does not exist
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 200 Success Response (Error is shown after using the URL provided)
+ *     https://storage.googleapis.com/example.appspot.com/assets/test.jpg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=example%40appspot.gserviceaccount.com{{url and key contents}}
+ */
