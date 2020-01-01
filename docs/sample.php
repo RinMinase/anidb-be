68ad43a44e764fe4107c12d94a26e91bb0c7fd6f
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-01 08:05:30 +0800

    DOCS: improved documentation template

--- docs/sample.php
+++ docs/sample.php
@@ -5,21 +5,21 @@
  * @apiName GetUser
  * @apiGroup Sample
  *
- * @apiParam {Number} id Users unique ID.
+ * @apiParam {Number} id Users unique ID
  *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
+ * @apiSuccess {String} firstname Firstname of the User
+ * @apiSuccess {String} lastname  Lastname of the User
  *
- * @apiSuccessExample Success-Response:
+ * @apiSuccessExample Success Response
  *     HTTP/1.1 200 OK
  *     {
  *       "firstname": "John",
  *       "lastname": "Doe"
  *     }
  *
- * @apiError UserNotFound The id of the User was not found.
+ * @apiError UserNotFound The id of the User was not found
  *
- * @apiErrorExample Error-Response:
+ * @apiErrorExample Error Response
  *     HTTP/1.1 404 Not Found
  *     {
  *       "error": "UserNotFound"
