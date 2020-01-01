71b9970ecbc35e6407335138c9047c46918bc0fa
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-02 07:13:13 +0800

    IMAGE: added verification of url, resolved #7

--- app/Controllers/ImageController.php
+++ app/Controllers/ImageController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use DateTime;
+use Exception;
+use GuzzleHttp\Client;
 
 class ImageController {
 
@@ -12,7 +14,19 @@ class ImageController {
 			->object(urldecode($param))
 			->signedUrl(new DateTime('tomorrow'), [ 'version' => 'v4' ]);
 
-		return response($data);
+		return response($this->verifyImageContents($data));
+	}
+
+	private function verifyImageContents($url) {
+		try {
+			$client = new Client();
+			$response = $client->request('GET', $url)->getHeaderLine('content-type');
+			$data = ($response == 'image/jpeg') ? $url : 'error';
+		} catch (Exception $e) {
+			$data = 'Image path is invalid';
+		}
+
+		return $data;
 	}
 
 }
