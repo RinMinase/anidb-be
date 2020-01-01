42e8ff63b831ae052a8852353278cd84419508c7
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-01 09:15:24 +0800

    DOCS_USER: added logout and register

--- docs/User.php
+++ docs/User.php
@@ -2,7 +2,6 @@
 
 /**
  * @api {post} /api/login User Login
- * @apiName login
  * @apiGroup User
  *
  * @apiHeader {String} api-key Back-end API Key
@@ -27,10 +26,103 @@
  * @apiError Unauthorized There is no api-key provided, or the api-key provided is invalid
  *
  * @apiErrorExample Invalid
- *     HTTP/1.1 404 Not Found
- *     "username" or "password" is invalid
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": "Invalid",
+ *       "message": "Username or Password is invalid"
+ *     }
  *
  * @apiErrorExample Unauthorized
- *     HTTP/1.1 404 Not Found
- *     "username" or "password" is invalids
+ *     HTTP/1.1 401 Forbidden
+ *     {
+ *       "status": "Unauthorized",
+ *       "message": "Unauthorized"
+ *     }
+ */
+
+/**
+ * @api {post} /api/logout User Logout
+ * @apiGroup User
+ *
+ * @apiHeader {String} api-key Back-end API Key
+ * @apiParam {String} token Token received from logging-in
+ *
+ * @apiSuccess {String} status Status code
+ * @apiSuccess {String} message Status message
+ *
+ * @apiSuccessExample Success Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "Success",
+ *       "message": "User has been logged-out"
+ *     }
+ *
+ * @apiError Invalid Login session is not found
+ * @apiError InvalidToken There is no token provided, or the token provided is invalid
+ * @apiError Unauthorized There is no api-key provided, or the api-key provided is invalid
+ *
+ * @apiErrorExample Invalid
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": "Invalid",
+ *       "message": "Username or Password is invalid"
+ *     }
+ *
+ * @apiErrorExample InvalidToken
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": "InvalidToken",
+ *       "message": "Session Token not found or invalid"
+ *     }
+ *
+ * @apiErrorExample Unauthorized
+ *     HTTP/1.1 401 Forbidden
+ *     {
+ *       "status": "Unauthorized",
+ *       "message": "Unauthorized"
+ *     }
+ */
+
+/**
+ * @api {post} /api/register User Registration
+ * @apiGroup User
+ *
+ * @apiHeader {String} api-key Back-end API Key
+ * @apiParam {String} username Username
+ * @apiParam {String} password Password
+ *
+ * @apiSuccess {String} status Status code
+ * @apiSuccess {String} message Status message
+ *
+ * @apiSuccessExample Success Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "Success",
+ *       "message": "User has been registered"
+ *     }
+ *
+ * @apiError Existing Username is already taken
+ * @apiError Invalid Either the Username or Password of the User is not provided
+ * @apiError Unauthorized There is no api-key provided, or the api-key provided is invalid
+ *
+ * @apiErrorExample Existing
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": "Existing",
+ *       "message": "Username is already taken"
+ *     }
+ *
+ * @apiErrorExample Invalid
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": "Invalid",
+ *       "message": "Username and Password fields are required"
+ *     }
+ *
+ * @apiErrorExample Unauthorized
+ *     HTTP/1.1 401 Forbidden
+ *     {
+ *       "status": "Unauthorized",
+ *       "message": "Unauthorized"
+ *     }
  */
