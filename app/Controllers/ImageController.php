343ee5991587f70077ff9fd10e4640423bcdd7de
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-02 07:13:39 +0800

    IMAGE: improved function call

--- app/Controllers/ImageController.php
+++ app/Controllers/ImageController.php
@@ -19,8 +19,7 @@ class ImageController {
 
 	private function verifyImageContents($url) {
 		try {
-			$client = new Client();
-			$response = $client->request('GET', $url)->getHeaderLine('content-type');
+			$response = (new Client())->get($url)->getHeaderLine('content-type');
 			$data = ($response == 'image/jpeg') ? $url : 'error';
 		} catch (Exception $e) {
 			$data = 'Image path is invalid';
