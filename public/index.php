4ad297d0b8fff066bc30542e88c7f60c1b47af3c
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-23 17:20:11 +0800

    ANIDB: added cors header

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 $app = require __DIR__.'/../bootstrap/app.php';
 $app->run();
