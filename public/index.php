d907d28c9d78b1742473d9bee45c9ed91ec51311
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-23 18:10:15 +0800

    ANIDB: removed cors policy in main file

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
-set_time_limit(0);
-
 $app = require __DIR__.'/../bootstrap/app.php';
 $app->run();
