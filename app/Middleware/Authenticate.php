b6e1786e8b0ca001d5f9a6ea1820320c37e893e0
Rin Minase <rin.black.raison.detre@gmail.com>
2020-03-27 17:46:52 +0800

    ANIDB: improved api response on authentication

--- app/Middleware/Authenticate.php
+++ app/Middleware/Authenticate.php
@@ -15,7 +15,10 @@ class Authenticate {
 
 	public function handle($request, Closure $next, $guard = null) {
 		if ($this->auth->guard($guard)->guest()) {
-			return response('Unauthorized', 401);
+			return response([
+				'status' => 401,
+				'message' => 'Unauthorized',
+			], 401);
 		}
 
 		return $next($request);
