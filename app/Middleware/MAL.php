2afc08b887b9f51e4724829b3c678febd54499ef
Rin Minase <rin.black.raison.detre@gmail.com>
2020-11-23 03:36:15 +0800

    MAL: fixed web crawler

--- app/Middleware/MAL.php
+++ app/Middleware/MAL.php
@@ -10,7 +10,8 @@ class MAL {
 
 	public function anime($id) {
 		try {
-			return Anime::parse(app('goutte')->request('get', '/anime/' . $id));
+			return Anime::parse(app('goutte')
+				->request('GET', 'https://' . env('SCRAPER_BASE_URI') . '/anime/' . $id));
 		} catch (Exception $e) {
 			if (env('APP_DEBUG')) {
 				throw new Exception('Issues in connecting to MAL Servers');
@@ -25,7 +26,12 @@ class MAL {
 
 	public function search($query) {
 		try {
-			return AnimeSearch::parse(app('goutte')->request('get', '/anime.php?q=' . urldecode($query)));
+			// Commented as Base URI is not currently working properly with Goutte
+			// https://github.com/FriendsOfPHP/Goutte/issues/427
+
+			return AnimeSearch::parse(app('goutte')
+				->request('GET', 'https://' . env('SCRAPER_BASE_URI') . '/anime.php?q=' . urldecode($query)));
+			// return AnimeSearch::parse(app('goutte')->request('GET', '/anime.php?q=' . urldecode($query)));
 		} catch (Exception $e) {
 			if (env('APP_DEBUG')) {
 				throw new Exception('Issues in connecting to MAL Servers');
