e1f7b9f7d990d669c900475d8e7f3e7e73a61220
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-02 00:12:47 +0800

    RELEASE: fixed error, improved code formatting

--- app/Controllers/ReleaseController.php
+++ app/Controllers/ReleaseController.php
@@ -21,13 +21,12 @@ class ReleaseController extends Controller {
   public function getLogs($limit = 20) {
     if ($this->isScraperEnabled()) {
       $data = Http::get($this->feReleaseURI . 'commits', [
-          'query' => [
-            'per_page' => $limit,
-          ],
-        ])
-        ->body();
+        'query' => [
+          'per_page' => $limit,
+        ],
+      ])->body();
 
-      $data = $this->parseChangelog($html);
+      $data = $this->parseChangelog($data);
 
       return response($data)
         ->header('Content-Type', 'application/json');
@@ -37,11 +36,10 @@ class ReleaseController extends Controller {
   public function getLogsBE($limit = 20) {
     if ($this->isScraperEnabled()) {
       $data = Http::get($this->beReleaseURI . 'commits', [
-          'query' => [
-            'per_page' => $limit,
-          ],
-        ])
-        ->body();
+        'query' => [
+          'per_page' => $limit,
+        ],
+      ])->body();
 
       $data = $this->parseChangelog($data);
 
@@ -53,12 +51,11 @@ class ReleaseController extends Controller {
   public function getIssues($limit = 100) {
     if ($this->isScraperEnabled()) {
       $data = Http::get($this->beReleaseURI . 'issues', [
-          'query' => [
-            'per_page' => $limit,
-            'page' => 1,
-          ],
-        ])
-        ->body();
+        'query' => [
+          'per_page' => $limit,
+          'page' => 1,
+        ],
+      ])->body();
 
       $data = $this->parseIssues($data);
 
@@ -78,140 +75,138 @@ class ReleaseController extends Controller {
   }
 
   private function parseChangelog($data) {
-		$changelog = json_decode($data);
-		$data = [];
+    $changelog = json_decode($data);
+    $data = [];
 
-		foreach ($changelog as $change) {
-			$rawDate = new DateTime($change->commit->author->date);
-			$parsedMessage = $this->parseChangelogMessage($change->commit->message);
+    foreach ($changelog as $change) {
+      $rawDate = new DateTime($change->commit->author->date);
+      $parsedMessage = $this->parseChangelogMessage($change->commit->message);
 
-			$data[] = [
-				'date' => $rawDate->format('M d, Y H:m'),
-				'email' => $change->commit->author->email,
-				'name' => $change->commit->author->name,
-				'message' => $parsedMessage->message,
-				'module' => $parsedMessage->module,
-				'url' => $change->url,
-			];
-		}
+      $data[] = [
+        'date' => $rawDate->format('M d, Y H:m'),
+        'email' => $change->commit->author->email,
+        'name' => $change->commit->author->name,
+        'message' => $parsedMessage->message,
+        'module' => $parsedMessage->module,
+        'url' => $change->url,
+      ];
+    }
 
-		return json_encode($this->categorizeChangelog($data));
-	}
+    return json_encode($this->categorizeChangelog($data));
+  }
 
   private function categorizeChangelog($changelog) {
-		$data = [];
-		$keywords = [
-			'dep' => [ 'dependency', 'dependencies' ],
-			'fix' => [ 'fixed', 'removed' ],
-			'new' => [ 'added', 'functional', 'migrated' ],
-		];
+    $data = [];
+    $keywords = [
+      'dep' => ['dependency', 'dependencies'],
+      'fix' => ['fixed', 'removed'],
+      'new' => ['added', 'functional', 'migrated'],
+    ];
 
-		foreach ($changelog as $change) {
-			$change = (object) $change;
+    foreach ($changelog as $change) {
+      $change = (object) $change;
 
-			if (strpos($change->message, 'Merge branch') === false) {
-				$commitDate = 'changes_' . (new DateTime($change->date))->format('Ymd');
-				$title = (new DateTime($change->date))->format('M d, Y');
+      if (strpos($change->message, 'Merge branch') === false) {
+        $commitDate = 'changes_' . (new DateTime($change->date))->format('Ymd');
+        $title = (new DateTime($change->date))->format('M d, Y');
 
-				if (!isset($data[$commitDate])) {
-					$data[$commitDate] = [
-						'dep' => [],
-						'fix' => [],
-						'new' => [],
-						'improve' => [],
-						'title' => $title,
-					];
-				}
+        if (!isset($data[$commitDate])) {
+          $data[$commitDate] = [
+            'dep' => [],
+            'fix' => [],
+            'new' => [],
+            'improve' => [],
+            'title' => $title,
+          ];
+        }
 
-				$isDep = $this->parseMessageType($change->message, $keywords['dep'])
-					&& $change->module === '';
-				$isFix = $this->parseMessageType($change->message, $keywords['fix']);
-				$isNew = $this->parseMessageType($change->message, $keywords['new']);
+        $isDep = $this->parseMessageType($change->message, $keywords['dep'])
+          && $change->module === '';
+        $isFix = $this->parseMessageType($change->message, $keywords['fix']);
+        $isNew = $this->parseMessageType($change->message, $keywords['new']);
 
-				if ($isDep) {
-					$data[$commitDate]['dep'][] = $change;
-				} else if ($isFix) {
-					$data[$commitDate]['fix'][] = $change;
-				} else if ($isNew) {
-					$data[$commitDate]['new'][] = $change;
-				} else {
-					$data[$commitDate]['improve'][] = $change;
-				}
+        if ($isDep) {
+          $data[$commitDate]['dep'][] = $change;
+        } else if ($isFix) {
+          $data[$commitDate]['fix'][] = $change;
+        } else if ($isNew) {
+          $data[$commitDate]['new'][] = $change;
+        } else {
+          $data[$commitDate]['improve'][] = $change;
+        }
+      }
+    }
 
-			}
-		}
+    return $data;
+  }
 
-		return $data;
-	}
+  private function parseChangelogMessage($message) {
+    $rawMessage = explode(':', $message);
 
-	private function parseChangelogMessage($message) {
-		$rawMessage = explode(':', $message);
+    if (count($rawMessage) == 1) {
+      $rawMessage = preg_split("/ (.+)/", $message);
+    }
 
-		if (count($rawMessage) == 1) {
-			$rawMessage = preg_split("/ (.+)/", $message);
-		}
+    if (strpos($rawMessage[1], ', resolved #') !== false) {
+      $rawMessage[1] = str_replace(', resolved #', '', $rawMessage[1]);
+    }
 
-		if (strpos($rawMessage[1], ', resolved #') !== false) {
-			$rawMessage[1] = str_replace(', resolved #', '', $rawMessage[1]);
-		}
+    $rawModule = ltrim(strtolower($rawMessage[0]));
+    $rawModule = str_replace('_', ' ', $rawModule);
 
-		$rawModule = ltrim(strtolower($rawMessage[0]));
-		$rawModule = str_replace('_', ' ', $rawModule);
+    $module = ($rawModule === 'anidb' || $rawModule === 'transition') ? '' : $rawModule;
+    $message = ltrim($rawMessage[1]);
 
-		$module = ($rawModule === 'anidb' || $rawModule === 'transition') ? '' : $rawModule;
-		$message = ltrim($rawMessage[1]);
+    return (object) [
+      'module' => $module,
+      'message' => $message,
+    ];
+  }
 
-		return (object) [
-			'module' => $module,
-			'message' => $message,
-		];
-	}
+  private function parseMessageType($message, $keywords) {
+    $value = false;
 
-	private function parseMessageType($message, $keywords) {
-		$value = false;
+    foreach ($keywords as $key) {
+      if (strpos($message, $key) !== false) {
+        $value = true;
+      }
+    }
 
-		foreach($keywords as $key) {
-			if (strpos($message, $key) !== false) {
-				$value = true;
-			}
-		}
+    return $value;
+  }
 
-		return $value;
-	}
+  private function parseIssues($issues) {
+    $issues = json_decode($issues);
+    $data = [];
 
-	private function parseIssues($issues) {
-		$issues = json_decode($issues);
-		$data = [];
+    foreach ($issues as $issue) {
+      if ($issue->state === 'open') {
+        $labels = [];
 
-		foreach ($issues as $issue) {
-			if ($issue->state === 'open') {
-				$labels = [];
+        foreach ($issue->labels as $label) {
+          if (!($label->name === 'todo' || $label->name === 'in progress')) {
+            $className = str_replace(':', '', $label->name);
+            $className = strtolower(str_replace(' ', '-', $className));
+            $labelName = strtoupper(explode(' ', $label->name)[1]);
 
-				foreach ($issue->labels as $label) {
-					if (!($label->name === 'todo' || $label->name === 'in progress')) {
-						$className = str_replace(':', '', $label->name);
-						$className = strtolower(str_replace(' ', '-', $className));
-						$labelName = strtoupper(explode(' ', $label->name)[1]);
+            $labels[] = [
+              'class' => $className,
+              'name' => $labelName,
+            ];
+          }
+        }
 
-						$labels[] = [
-							'class' => $className,
-							'name' => $labelName,
-						];
-					}
-				}
+        $labels = array_reverse($labels);
+        $data[] = [
+          'date' => (new DateTime($issue->created_at))->format('M d, Y'),
+          'labels' => $labels,
+          'number' => $issue->number,
+          'title' => $issue->title,
+          'url' => $issue->html_url,
+        ];
+      }
+    }
 
-				$labels = array_reverse($labels);
-				$data[] = [
-					'date' => (new DateTime($issue->created_at))->format('M d, Y'),
-					'labels' => $labels,
-					'number' => $issue->number,
-					'title' => $issue->title,
-					'url' => $issue->html_url,
-				];
-			}
-		}
-
-		return $data;
-	}
-
+    return $data;
+  }
 }
