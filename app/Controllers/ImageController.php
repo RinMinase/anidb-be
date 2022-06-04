6dd1cd8f77783fc82ddb08af3f831542651d2029
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-04 23:20:39 +0800

    IMAGE: moved docs to controller

--- app/Controllers/ImageController.php
+++ app/Controllers/ImageController.php
@@ -32,6 +32,31 @@ class ImageController extends Controller {
       -> createStorage();
   }
 
+  /**
+   * @api {get} /api/img/:param Retrieve Image URL
+   * @apiName ImageRetrieve
+   * @apiGroup Image
+   *
+   * @apiHeader {String} token User login token
+   * @apiParam {Path} param Complete image file path (i.e. 'assets/test.jpg')
+   *
+   * @apiSuccess {String} url Signed URL of image
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "url": "https://storage.googleapis.com/example.appspot.com/assets/test.jpg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=example%40appspot.gserviceaccount.com{{url and key contents}}"
+   *     }
+   *
+   * @apiError Invalid The specified image path does not exist
+   *
+   * @apiErrorExample Invalid
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "status": "Invalid",
+   *       "message": "Image path is invalid"
+   *     }
+   */
   public function index($params) {
     if (!env('DISABLE_FIREBASE')) {
       if (
