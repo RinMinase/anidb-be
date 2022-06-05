2cc4e86013537dbbecb869dca6d0f0f63e25117f
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-05 17:27:25 +0800

    ANIDB: fixes to code formatting

--- database/migrations/2022_06_05_080624_create_marathons_table.php
+++ database/migrations/2022_06_05_080624_create_marathons_table.php
@@ -4,31 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('marathons', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->date('date_from');
-            $table->date('date_to');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('marathons', function (Blueprint $table) {
+      $table->id();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('marathons');
-    }
+      $table->string('title');
+      $table->date('date_from');
+      $table->date('date_to');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('marathons');
+  }
 };
--- database/seeders/MarathonSeeder.php
+++ database/seeders/MarathonSeeder.php
@@ -6,39 +6,38 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
-class MarathonSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      $testData = [
-        [
-          'title' => 'Summer 2013',
-          'date_from' => Carbon::parse('2013-03-24')->format('Y-m-d'),
-          'date_to' => Carbon::parse('2013-06-09')->format('Y-m-d'),
-          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-          'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ],
-        [
-          'title' => 'Summer 2014',
-          'date_from' => Carbon::parse('2014-04-01')->format('Y-m-d'),
-          'date_to' => Carbon::parse('2014-06-15')->format('Y-m-d'),
-          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-          'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ],
-        [
-          'title' => 'Summer 2015',
-          'date_from' => Carbon::parse('2015-03-29')->format('Y-m-d'),
-          'date_to' => Carbon::parse('2015-06-08')->format('Y-m-d'),
-          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-          'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ],
-      ];
+class MarathonSeeder extends Seeder {
 
-      DB::table('marathons')->insert($testData);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    $testData = [
+      [
+        'title' => 'Summer 2013',
+        'date_from' => Carbon::parse('2013-03-24')->format('Y-m-d'),
+        'date_to' => Carbon::parse('2013-06-09')->format('Y-m-d'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+      [
+        'title' => 'Summer 2014',
+        'date_from' => Carbon::parse('2014-04-01')->format('Y-m-d'),
+        'date_to' => Carbon::parse('2014-06-15')->format('Y-m-d'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+      [
+        'title' => 'Summer 2015',
+        'date_from' => Carbon::parse('2015-03-29')->format('Y-m-d'),
+        'date_to' => Carbon::parse('2015-06-08')->format('Y-m-d'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+    ];
+
+    DB::table('marathons')->insert($testData);
+  }
 }
