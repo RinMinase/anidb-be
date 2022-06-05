a76a9fc07f6d49304363adfded484f482a615aec
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-05 15:13:53 +0800

    HDD: added migration

---
+++ database/migrations/2022_06_05_064848_create_hdd_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('hdd', function (Blueprint $table) {
+      $table->id();
+
+      $table->char('from', 1);
+      $table->char('to', 1);
+      $table->bigInteger('size');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('hdd');
+  }
+};
---
+++ database/seeders/HddSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class HddSeeder extends Seeder {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    $testData = [
+      [
+        'from' => 'a',
+        'to' => 'd',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ], [
+        'from' => 'e',
+        'to' => 'h',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ], [
+        'from' => 'i',
+        'to' => 'l',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ], [
+        'from' => 'm',
+        'to' => 'p',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ], [
+        'from' => 'q',
+        'to' => 'u',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ], [
+        'from' => 'v',
+        'to' => 'z',
+        'size' => 2_000_339_066_880,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+    ];
+
+    DB::table('hdd')->insert($testData);
+  }
+}
