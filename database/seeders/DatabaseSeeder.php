94a37f23455c092ba7254ff81ac45200ef24223c
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-05 17:41:04 +0800

    LOGS: added migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder {
 
     $this->call([
       HddSeeder::class,
+      LogSeeder::class,
       MarathonSeeder::class,
     ]);
   }
---
+++ database/seeders/LogSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class LogSeeder extends Seeder {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    $testData = [
+      [
+        'table_changed' => 'hdd',
+        'id_changed' => 1,
+        'description' => 'some description',
+        'action' => 'add',
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+      [
+        'table_changed' => 'summer',
+        'id_changed' => 1,
+        'description' => 'title from "old" to "new"',
+        'action' => 'edit',
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ],
+    ];
+
+    DB::table('logs')->insert($testData);
+  }
+}
