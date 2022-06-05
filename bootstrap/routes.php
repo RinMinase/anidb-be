3d53c31609a57d3a408a98ef668e9e0823078570
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-06 03:02:56 +0800

    ANIDB: appended route names

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')
   ->group(function () {
-    Route::view('/', 'index');
+    Route::view('/', 'index')->name('home');
   });
 
 Route::prefix('api')
@@ -16,7 +16,7 @@ Route::prefix('api')
     Route::prefix('auth')
       ->group(function () {
         Route::post('register', 'AuthController@register');
-        Route::post('login', 'AuthController@login');
+        Route::post('login', 'AuthController@login')->name('login');
 
         Route::middleware('auth:sanctum')
           ->group(function () {
