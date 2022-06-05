40628456b92f323703ccc7ddc021264397dc33f5
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-05 16:43:22 +0800

    MARA: added api docs

--- app/Controllers/MarathonController.php
+++ app/Controllers/MarathonController.php
@@ -13,6 +13,43 @@ class MarathonController extends Controller {
     $this->marathonRepository = $marathonRepository;
   }
 
+  /**
+   * @api {get} /api/marathon Retrieve all Marathons
+   * @apiName MarathonRetrieve
+   * @apiGroup Marathon
+   *
+   * @apiHeader {String} token User login token
+   *
+   * @apiSuccess {Object[]} data Marathon Data
+   * @apiSuccess {Number} data.id ID of the marathon entry
+   * @apiSuccess {String} data.title Descriptive title of the marathon
+   * @apiSuccess {String} data.date_from Start date of the marathon
+   * @apiSuccess {Number} data.date_to End date of the marathon
+   * @apiSuccess {DateTime} data.created_at Creation date of the marathon entry
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "data": [
+   *         {
+   *           id: 1
+   *           title: "Summer List",
+   *           date_from: "2020-01-01",
+   *           date_to: "2020-02-01",
+   *           created_at: "2020-01-01 00:00:00",
+   *         }
+   *       ]
+   *     }
+   *
+   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   *
+   * @apiErrorExample Unauthorized
+   *     HTTP/1.1 401 Forbidden
+   *     {
+   *       "status": "Unauthorized",
+   *       "message": "Unauthorized"
+   *     }
+   */
   public function index(): JsonResponse {
     return response()->json([
       'data' => $this->marathonRepository->getAll(),
