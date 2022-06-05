ffac7d4eeed6257f298c487d2fde3d962ae739de
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-06 02:48:27 +0800

    ANIDB: disable invalid routes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,40 +2,44 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+class Handler extends ExceptionHandler {
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array<int, class-string<Throwable>>
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array<int, string>
+   */
+  protected $dontFlash = [
+    'current_password',
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+  /**
+   * Register the exception handling callbacks for the application.
+   *
+   * @return void
+   */
+  public function register() {
+    $this->reportable(function (Throwable $e) {
+      //
+    });
+  }
+
+  public function render($request, Throwable $e) {
+    abort_if($e instanceof MethodNotAllowedHttpException, 404);
+    return parent::render($request, $e);
+  }
 }
