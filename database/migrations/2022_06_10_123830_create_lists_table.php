27d0f7dac4f779ec1bf097b0099cf099c114e421
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-10 20:46:50 +0800

    CATALOG: created migrations

---
+++ database/migrations/2022_06_10_123830_create_lists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('lists', function (Blueprint $table) {
+      $table->id();
+
+      $table->string('description', 16);
+
+      $table->smallInteger('year')->nullable();
+      $table->enum(
+        'season',
+        ['Winter', 'Spring', 'Summer', 'Fall'],
+      )->nullable();
+
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('lists');
+  }
+};
