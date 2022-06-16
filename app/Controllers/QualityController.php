2c9dde0bd02af5b7e87f7ac470be667106f04fa2
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-16 15:35:09 +0800

    ANIDB: tested swagger docs generation

--- app/Controllers/QualityController.php
+++ app/Controllers/QualityController.php
@@ -9,12 +9,46 @@ use App\Resources\Quality\QualityCollection;
 
 class QualityController extends Controller {
 
+  /**
+   * @OA\Info(
+   *      version="1.0.0",
+   *      title="Quality",
+   *      description="Description",
+   * )
+   *
+   * @OA\Server(
+   *      url=L5_SWAGGER_CONST_HOST,
+   * )
+   */
+
   private QualityRepository $qualityRepository;
 
   public function __construct(QualityRepository $qualityRepository) {
     $this->qualityRepository = $qualityRepository;
   }
 
+  /**
+   * @OA\Get(
+   *   path="/quality",
+   *   description="Retrieve Quality List",
+   *   @OA\Response(
+   *    response=200,
+   *    description="Success"
+   *   ),
+   *   @OA\Response(
+   *     response=400,
+   *     description="Bad Request"
+   *   ),
+   *   @OA\Response(
+   *     response=401,
+   *     description="Unauthenticated",
+   *   ),
+   *   @OA\Response(
+   *     response=403,
+   *     description="Forbidden"
+   *   )
+   * )
+   */
   public function index(): JsonResponse {
     return response()->json([
       'data' => QualityCollection::collection($this->qualityRepository->getAll()),
