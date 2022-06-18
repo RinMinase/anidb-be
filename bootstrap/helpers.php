763c9190b135b7aae4a36de35861b34cf557afb0
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-19 01:44:09 +0800

    ANIDB: added helper function for filesize parsing

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,7 +1,21 @@
 <?php
 
-if (! function_exists('test_function')) {
-  function test_function() {
-    return "Example helper function";
+if (!function_exists('parse_filesize')) {
+  function parse_filesize(int $size): string {
+    $KB = 1024;
+    $MB = 1024 * $KB;
+    $GB = 1024 * $MB;
+
+    if ($size >= $GB) {
+      $filesize = round($size / $GB, 2) . " GB";
+    } else if ($size >= $MB) {
+      $filesize = round($size / $MB, 2) . " MB";
+    } else if ($size >= $KB) {
+      $filesize = round($size / $KB, 2) . " KB";
+    } else {
+      $filesize = $size ?? 0 . " B";
+    }
+
+    return $filesize;
   }
 }
