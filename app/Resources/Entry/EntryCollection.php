2562e7852abe322a601bdf1e212db183e5628170
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-19 01:50:14 +0800

    HOME: modified collection query for readability

--- app/Resources/Entry/EntryCollection.php
+++ app/Resources/Entry/EntryCollection.php
@@ -9,19 +9,23 @@ class EntryCollection extends JsonResource {
 
   public function toArray($request) {
 
-    /**
-     * Rating Computation
-     */
-    $rating = 0;
-    $rating += $this->rating ? $this->rating->audio : 0;
-    $rating += $this->rating ? $this->rating->enjoyment : 0;
-    $rating += $this->rating ? $this->rating->graphics : 0;
-    $rating += $this->rating ? $this->rating->plot : 0;
-    $rating = round($rating / 4, 2);
+    return [
+      'id' => $this->id,
+      'quality' => $this->quality->quality,
+      'title' => $this->title,
+      'dateFinished' => $this->calcDateFinished(),
+      'filesize' => parse_filesize($this->filesize ?? 0),
+      'episodes' => $this->episodes ?? 0,
+      'ovas' => $this->ovas ?? 0,
+      'specials' => $this->specials ?? 0,
+      'encoder' => $this->calcEncoder(),
+      'release' => $this->calcRelease(),
+      'remarks' => $this->remarks,
+      'rating' => $this->calcRating(),
+    ];
+  }
 
-    /**
-     * Last Date Finished Computation
-     */
+  private function calcDateFinished() {
     $last_date_finished = '';
 
     if ($this->date_finished) {
@@ -36,52 +40,31 @@ class EntryCollection extends JsonResource {
         ->format('M d, Y');
     }
 
-    /**
-     * Filesize Computation
-     */
-    $KB = 1024;
-    $MB = 1024 * $KB;
-    $GB = 1024 * $MB;
+    return $last_date_finished;
+  }
 
-    if ($this->filesize >= $GB) {
-      $filesize = round($this->filesize / $GB, 2) . " GB";
-    } else if ($this->filesize >= $MB) {
-      $filesize = round($this->filesize / $MB, 2) . " MB";
-    } else if ($this->filesize >= $KB) {
-      $filesize = round($this->filesize / $KB, 2) . " KB";
-    } else {
-      $filesize = $this->filesize ?? 0 . " B";
-    }
-
-    /**
-     * Encoder Computation
-     */
+  private function calcEncoder() {
     $encoders = [];
 
     if ($this->encoder_video) $encoders[] = $this->encoder_video;
     if ($this->encoder_audio) $encoders[] = $this->encoder_audio;
     if ($this->encoder_subs) $encoders[] = $this->encoder_subs;
 
-    $encoder = join('—', $encoders);
+    return join('—', $encoders);
+  }
 
-    /**
-     * Release Computation
-     */
-    $release = trim($this->release_season . ' ' . $this->release_year);
+  private function calcRating() {
+    $rating = 0;
+    $rating += $this->rating->audio ?? 0;
+    $rating += $this->rating->enjoyment ?? 0;
+    $rating += $this->rating->graphics ?? 0;
+    $rating += $this->rating->plot ?? 0;
+    $rating = round($rating / 4, 2);
 
-    return [
-      'id' => $this->id,
-      'quality' => $this->quality->quality,
-      'title' => $this->title,
-      'dateFinished' => $last_date_finished,
-      'filesize' => $filesize,
-      'episodes' => $this->episodes,
-      'ovas' => $this->ovas,
-      'specials' => $this->specials,
-      'encoder' => $encoder,
-      'release' => $release,
-      'remarks' => $this->remarks,
-      'rating' => $rating,
-    ];
+    return $rating;
+  }
+
+  private function calcRelease() {
+    return trim($this->release_season . ' ' . $this->release_year);
   }
 }
