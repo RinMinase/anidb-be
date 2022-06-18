1779b357f923d0a06ef507d90883b66a64cd7f0d
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-19 01:03:15 +0800

    HOME: modified collection query

--- app/Repositories/EntryRepository.php
+++ app/Repositories/EntryRepository.php
@@ -8,8 +8,6 @@ class EntryRepository {
 
   public function getAll() {
     return Entry::select()
-      ->with('offquels')
-      ->with('rewatches')
       ->with('rating')
       ->get();
   }
--- app/Resources/Entry/EntryCollection.php
+++ app/Resources/Entry/EntryCollection.php
@@ -3,10 +3,15 @@
 namespace App\Resources\Entry;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class EntryCollection extends JsonResource {
 
   public function toArray($request) {
+
+    /**
+     * Rating Computation
+     */
     $rating = 0;
     $rating += $this->rating ? $this->rating->audio : 0;
     $rating += $this->rating ? $this->rating->enjoyment : 0;
@@ -14,28 +19,68 @@ class EntryCollection extends JsonResource {
     $rating += $this->rating ? $this->rating->plot : 0;
     $rating = round($rating / 4, 2);
 
+    /**
+     * Last Date Finished Computation
+     */
+    $last_date_finsihed = '';
+
+    if ($this->date_finished) {
+      $last_date_finsihed = Carbon::parse($this->date_finished)
+        ->format('M d, Y');
+    }
+
+    if (count($this->rewatches)) {
+      $len = count($this->rewatches);
+      $last_date = $this->rewatches[$len - 1]->date_rewatched;
+      $last_date_finsihed = Carbon::parse($last_date)
+        ->format('M d, Y');
+    }
+
+    /**
+     * Filesize Computation
+     */
+    $KB = 1024;
+    $MB = 1024 * $KB;
+    $GB = 1024 * $MB;
+
+    if ($this->filesize >= $GB) {
+      $filesize = round($this->filesize / $GB, 2) . " GB";
+    } else if ($this->filesize >= $MB) {
+      $filesize = round($this->filesize / $MB, 2) . " MB";
+    } else if ($this->filesize >= $KB) {
+      $filesize = round($this->filesize / $KB, 2) . " KB";
+    } else {
+      $filesize = $this->filesize ?? 0 . " B";
+    }
+
+    /**
+     * Encoder Computation
+     */
+    $encoders = [];
+
+    if ($this->encoder_video) $encoders[] = $this->encoder_video;
+    if ($this->encoder_audio) $encoders[] = $this->encoder_audio;
+    if ($this->encoder_subs) $encoders[] = $this->encoder_subs;
+
+    $encoder = join('—', $encoders);
+
+    /**
+     * Release Computation
+     */
+    $release = trim($this->release_season . ' ' . $this->release_year);
+
     return [
+      'id' => $this->id,
       'quality' => $this->quality->quality,
       'title' => $this->title,
-      'date_finished' => $this->date_finished,
-      'duration' => $this->duration,
-      'filesize' => $this->filesize,
+      'dateFinished' => $last_date_finsihed,
+      'filesize' => $filesize,
       'episodes' => $this->episodes,
       'ovas' => $this->ovas,
       'specials' => $this->specials,
-      'season_number' => $this->season_number,
-      'season_first_title' => $this->season_first_title,
-      'prequel' => $this->prequel,
-      'sequel' => $this->sequel,
-      'encoder_video' => $this->encoder_video,
-      'encoder_audio' => $this->encoder_audio,
-      'encoder_subs' => $this->encoder_subs,
-      'release_year' => $this->release_year,
-      'release_season' => $this->release_season,
-      'variants' => $this->variants,
+      'encoder' => $encoder,
+      'release' => $release,
       'remarks' => $this->remarks,
-      'offquels' => EntryOffquelCollection::collection($this->offquels),
-      'rewatches' => $this->rewatches,
       'rating' => $rating,
     ];
   }
