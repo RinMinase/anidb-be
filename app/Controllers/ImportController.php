2e368900e85c807b5a2c0e3804525977718cd462
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-25 06:12:49 +0800

    IMPORT: added partial import for local relationship keys

--- app/Controllers/ImportController.php
+++ app/Controllers/ImportController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use Exception;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 
@@ -38,6 +39,8 @@ class ImportController extends Controller {
           'remarks' => $item['remarks'] ?? null,
           'variants' => $item['variants'] ?? null,
 
+          'season_number' => $item['seasonNumber'] ?? null,
+
           'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
           'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ];
@@ -47,29 +50,94 @@ class ImportController extends Controller {
 
       Entry::insert($import);
 
-      $importRatings = [];
+      $import_updates = [];
+      $import_updates_ids = [];
+      $import_ratings = [];
+      $id_entries = DB::table('entries')
+        ->select('id', 'title')
+        ->get()
+        ->toArray();
 
       // second run-through for all foreign keys
       foreach ($request->all() as $item) {
+        $index = null;
+
+        /**
+         * If it contains ratings
+         */
         if (
           $item['rating']['audio']
           || $item['rating']['enjoyment']
           || $item['rating']['graphics']
           || $item['rating']['plot']
         ) {
-          $id_entry = Entry::where('title', $item['title'])->first()->id;
+          $index = array_column($id_entries, 'title');
+          $index = array_search($item['title'], $index);
 
-          array_push($importRatings, [
-            'id_entries' => $id_entry,
+          array_push($import_ratings, [
+            'id_entries' => $index,
             'audio' => $item['rating']['audio'],
             'enjoyment' => $item['rating']['enjoyment'],
             'graphics' => $item['rating']['graphics'],
             'plot' => $item['rating']['plot'],
           ]);
         }
+
+        /**
+         * Checking for local relationship keys
+         */
+        if (
+          $item['firstSeasonTitle']
+          || $item['prequel']
+          || $item['sequel']
+        ) {
+          if (!$index) {
+            $index = array_column($id_entries, 'title');
+            $index = array_search($item['title'], $index);
+          }
+
+          $first_title_id = null;
+          $prequel_id = null;
+          $sequel_id = null;
+
+          foreach ($id_entries as $entry) {
+            if (
+              $item['firstSeasonTitle']
+              && $entry->title === $item['firstSeasonTitle']
+            ) {
+              $first_title_id = $entry->id;
+            }
+
+            if ($item['prequel'] && $entry->title === $item['prequel']) {
+              $prequel_id = $entry->id;
+            }
+
+            if ($item['sequel'] && $entry->title === $item['sequel']) {
+              $sequel_id = $entry->id;
+            }
+          }
+
+          array_push($import_updates_ids, $index + 1);
+          array_push($import_updates, [
+            'season_first_title_id' => $first_title_id,
+            'prequel_id' => $prequel_id,
+            'sequel_id' => $sequel_id,
+          ]);
+        }
       }
 
-      EntryRating::insert($importRatings);
+      /**
+       * Loops the 'for update' array
+       */
+      foreach ($import_updates as $key => $import_update) {
+        Entry::where('id', $import_updates_ids[$key])
+          ->update($import_update);
+      }
+
+      /**
+       * Insert ratings
+       */
+      EntryRating::insert($import_ratings);
 
       return response()->json([
         'status' => 200,
