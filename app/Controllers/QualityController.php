8a4f7d065855405137a1da37170f68dda19481f7
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-25 02:55:52 +0800

    ANIDB: modified quality api request

--- app/Controllers/QualityController.php
+++ app/Controllers/QualityController.php
@@ -5,7 +5,6 @@ namespace App\Controllers;
 use Illuminate\Http\JsonResponse;
 
 use App\Repositories\QualityRepository;
-use App\Resources\Quality\QualityCollection;
 
 class QualityController extends Controller {
 
@@ -15,9 +14,34 @@ class QualityController extends Controller {
     $this->qualityRepository = $qualityRepository;
   }
 
+
+  /**
+   * @api {get} /api/qualities Retrieve all quality
+   * @apiName QualityRetrieve
+   * @apiGroup Qualities
+   *
+   * @apiHeader {String} Authorization Token received from logging-in
+   *
+   * @apiSuccess {Object[]} data Quality Array
+   * @apiSuccess {Number} data.id Quality ID
+   * @apiSuccess {String} data.quality Quality description
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "data": [
+   *         {
+   *           "id": 1,
+   *           "quality": "4K 2160p"
+   *         }, { ... }
+   *       ]
+   *     }
+   *
+   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   */
   public function index(): JsonResponse {
     return response()->json([
-      'data' => QualityCollection::collection($this->qualityRepository->getAll()),
+      'data' => $this->qualityRepository->getAll(),
     ]);
   }
 }
