a13d9ccad4f6ea6f7fb87b3264e7b79abfc1ed7b
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-25 04:50:40 +0800

    IMPORT: added partial import functionality

--- app/Controllers/ImportController.php
+++ app/Controllers/ImportController.php
@@ -17,12 +17,31 @@ class ImportController extends Controller {
       $import = [];
 
       foreach ($request->all() as $item) {
-        array_push($import, [
+        $data = [
           'uuid' => Str::uuid()->toString(),
+
+          'id_quality' => $this->parse_quality($item['quality']),
           'title' => $item['title'] ?? null,
+
+          'date_finished' => Carbon::parse('@' . $item['dateFinished'])->format('Y-m-d'),
+          'duration' => $item['duration'] ?? 0,
+          'filesize' => $item['filesize'] ?? 0,
+
+          'episodes' => $item['episodes'] ?? 0,
+          'ovas' => $item['ovas'] ?? 0,
+          'specials' => $item['specials'] ?? 0,
+
+          'release_season' => $this->parse_season($item['releaseSeason']) ?? 0,
+          'release_year' => $item['releaseYear'],
+
           'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
           'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
+        ];
+
+        $item['remarks'] ? $data['remarks'] = $item['remarks'] : null;
+        $item['variants'] ? $data['variants'] = $item['variants'] : null;
+
+        array_push($import, $data);
       }
 
       Entry::insert($import);
@@ -31,11 +50,41 @@ class ImportController extends Controller {
         'status' => 200,
         'message' => 'Success',
       ]);
-    } catch (Exception) {
-      return response()->json([
-        'status' => 401,
-        'message' => 'Failed to import JSON file',
-      ]);
+    } catch (Exception $e) {
+      throw $e;
+      // return response()->json([
+      //   'status' => 401,
+      //   'message' => 'Failed to import JSON file',
+      // ]);
+    }
+  }
+
+  private function parse_quality(string $quality): int {
+    switch ($quality) {
+      case '4K 2160p':
+        return 1;
+      case 'FHD 1080p':
+        return 2;
+      case 'HD 720p':
+        return 3;
+      case 'HQ 480p':
+        return 4;
+      case 'LQ 360p':
+        return 5;
+      default:
+        return null;
+    }
+  }
+
+  private function parse_season(string $season): string {
+    switch ($season) {
+      case 'Winter':
+      case 'Spring':
+      case 'Summer':
+      case 'Fall':
+        return $season;
+      default:
+        return null;
     }
   }
 }
