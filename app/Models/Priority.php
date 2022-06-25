108a0379b0feb7b1701beb1445410bcf2f045474
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-25 21:38:46 +0800

    ANIDB: added priority model

---
+++ app/Models/Priority.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Priority extends Model {
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [];
+}
