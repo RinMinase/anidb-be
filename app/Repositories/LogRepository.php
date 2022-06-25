faf81a886b93828e1dcb435c6d9b81d9538b718a
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-26 01:08:08 +0800

    LOG: added function for creating logs

--- app/Repositories/LogRepository.php
+++ app/Repositories/LogRepository.php
@@ -2,6 +2,9 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
 use App\Models\Log;
 
 class LogRepository {
@@ -10,5 +13,23 @@ class LogRepository {
     return Log::orderBy('created_at', 'desc')
       ->orderBy('id', 'asc')
       ->get();
+  }
+
+  public static function generateLogs(
+    $table_changed = null,
+    $id_changed = null,
+    $desc = null,
+    $action = null,
+  ) {
+    $data = [
+      'uuid' => Str::uuid()->toString(),
+      'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      'table_changed' => $table_changed,
+      'id_changed' => $id_changed,
+      'desc' => $desc,
+      'action' => $action,
+    ];
+
+    Log::create($data);
   }
 }
