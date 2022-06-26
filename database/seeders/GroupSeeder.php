4a718f8dcb062b2da0d12845d60c494cc54cd3b2
Rin Minase <rin.black.raison.detre@gmail.com>
2022-06-26 17:42:36 +0800

    GROUP: added seeders

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -13,22 +13,68 @@ class GroupSeeder extends Seeder {
    * @return void
    */
   public function run() {
-    $testData = [
-      [
-        'name' => 'Group 1',
-        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-      ], [
-        'name' => 'Group 2',
-        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-      ], [
-        'title' => 'Group 3',
-        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-      ],
+    $data = [];
+    $groups = [
+      'A&C',
+      'Anbu',
+      'ANE',
+      'AonE',
+      'Asakura',
+      'Asenshi',
+      'Beatrice-Raws',
+      'BlurayDesuYo',
+      'Chihiro',
+      'Coalgirls',
+      'Commie',
+      'Cyan',
+      'DameDesuYo',
+      'DeadNews',
+      'DmonHiro',
+      'Doki',
+      'Erai-raws',
+      'EMBER',
+      'GSK_kun',
+      'HorribleSubs',
+      'iAHD',
+      'IrizaRaws',
+      'Kametsu',
+      'kBaraka',
+      'LostYears',
+      'LowPower-Raws',
+      'Moozzi2',
+      'MTBB',
+      'neko-raws',
+      'Nep_Blanc',
+      'Nii-sama',
+      'Project-GXS',
+      'Rasetsu',
+      'Raws-Maji',
+      'Reinforce',
+      'sam',
+      'SallySubs',
+      'sergey_krs',
+      'SCP-2223',
+      'ShowY',
+      'Snow-Raws',
+      'SubsPlease',
+      'THORA',
+      'tlacatlc6',
+      'Tsundere',
+      'UCCUSS',
+      'VCB-Studio',
+      'Vivid',
+      'Yousei-raws',
+      'Zurako',
     ];
 
-    DB::table('groups')->insert($testData);
+    foreach ($groups as $group) {
+      array_push($data, [
+        'name' => $group,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ]);
+    }
+
+    DB::table('groups')->insert($data);
   }
 }
