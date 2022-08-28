1a38ccb3103874a259af249abe2ee711970f6c0b
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-28 17:55:09 +0800

    📝 docs(sim): added api docs for all and single get queries

--- app/Controllers/BucketSimController.php
+++ app/Controllers/BucketSimController.php
@@ -15,12 +15,85 @@ class BucketSimController extends Controller {
     $this->bucketSimRepository = $bucketSimRepository;
   }
 
+
+  /**
+   * @api {get} /api/bucket-sims Retrieve All Bucket Sims
+   * @apiName RetrieveAllBucketSim
+   * @apiGroup BucketSim
+   *
+   * @apiHeader {String} token User login token
+   *
+   * @apiSuccess {Object[]} data Bucket Simulation list
+   * @apiSuccess {UUID} data.id Entry ID
+   * @apiSuccess {String} data.description Bucket Sim description
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "uuid": 9ef81943-78f0-4d1c-a831-a59fb5af339c,
+   *         "description": "Some bucket sim",
+   *       }, { ... }
+   *     ]
+   *
+   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   */
   public function index(): JsonResponse {
     return response()->json([
       'data' => $this->bucketSimRepository->getAll(),
     ]);
   }
 
+
+  /**
+   * @api {get} /api/bucket-sims/:id Retrieve Bucket Sim
+   * @apiName RetrieveBucketSim
+   * @apiGroup BucketSim
+   *
+   * @apiHeader {String} token User login token
+   * @apiParam {String} id Bucket Simulation ID
+   *
+   * @apiSuccess {Object[]} data Buckets with entries data
+   * @apiSuccess {UUID} data.id Entry ID
+   * @apiSuccess {String} data.from Filesize in nearest byte unit
+   * @apiSuccess {String} data.free Filesize in nearest byte unit
+   * @apiSuccess {String} data.freeTB Filesize in nearest byte unit
+   * @apiSuccess {String} data.used Filesize in nearest byte unit
+   * @apiSuccess {Number} data.percent Filesize in nearest byte unit
+   * @apiSuccess {String} data.total Filesize in nearest byte unit
+   * @apiSuccess {Number} data.rawTotal Filesize in nearest byte unit
+   * @apiSuccess {Number} data.titles Filesize in nearest byte unit
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "id": null,
+   *         "from": null,
+   *         "to": null,
+   *         "free": "1.11 TB",
+   *         "freeTB": "1.11 TB",
+   *         "used": "123.12 GB",
+   *         "percent": 10,
+   *         "total": "1.23 TB",
+   *         "rawTotal": 1000169533440,
+   *         "titles": 1
+   *       }, {
+   *         "id": 1,
+   *         "from": "a",
+   *         "to": "d",
+   *         "free": "1.11 TB",
+   *         "freeTB": null,
+   *         "used": "123.12 GB",
+   *         "percent": 10,
+   *         "total": "1.23 TB",
+   *         "rawTotal": 1000169533440,
+   *         "titles": 1
+   *       }, { ... }
+   *     ]
+   *
+   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   */
   public function get($uuid): JsonResponse {
     return response()->json([
       'data' => $this->bucketSimRepository->get($uuid),
