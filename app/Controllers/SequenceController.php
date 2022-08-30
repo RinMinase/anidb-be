98236f4c82ba5aedec993fddcd29037d3763e560
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-31 00:14:00 +0800

    📝 docs(sequence): added api documentation

--- app/Controllers/SequenceController.php
+++ app/Controllers/SequenceController.php
@@ -41,7 +41,6 @@ class SequenceController extends Controller {
    *           title: "Summer List",
    *           date_from: "2020-01-01",
    *           date_to: "2020-02-01",
-   *           created_at: "2020-01-01 00:00:00",
    *         }
    *       ]
    *     }
@@ -54,6 +53,35 @@ class SequenceController extends Controller {
     ]);
   }
 
+
+  /**
+   * @api {get} /api/sequences/:id Retrieve all Sequences
+   * @apiName SequenceRetrieve
+   * @apiGroup Sequence
+   *
+   * @apiHeader {String} Authorization Token received from logging-in
+   * @apiParam {Number} id Sequence ID
+   *
+   * @apiSuccess {Object} data Sequence Data
+   * @apiSuccess {Number} data.id ID of the sequence entry
+   * @apiSuccess {String} data.title Descriptive title of the sequence
+   * @apiSuccess {String} data.date_from Start date of the sequence
+   * @apiSuccess {Number} data.date_to End date of the sequence
+   *
+   * @apiSuccessExample Success Response
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "data": {
+   *         id: 1
+   *         title: "Summer List",
+   *         date_from: "2020-01-01",
+   *         date_to: "2020-02-01",
+   *         created_at: "2020-01-01 00:00:00",
+   *       }
+   *     }
+   *
+   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   */
   public function get($id): JsonResponse {
     return response()->json([
       'data' => $this->sequenceRepository->get($id),
