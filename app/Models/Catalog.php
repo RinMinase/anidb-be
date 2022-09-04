9b53f2c8e17a2e684c333a9a62e2b1ebd21faf8a
Rin Minase <rin.black.raison.detre@gmail.com>
2022-09-05 01:58:40 +0800

    ✨ feat(catalog): removed soft deletes

--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -3,11 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Catalog extends Model {
-
-  use SoftDeletes;
 
   /**
    * The attributes that are mass assignable.
