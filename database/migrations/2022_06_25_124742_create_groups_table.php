98c768d64c82c5444f727f29258120db3050ce85
Rin Minase <rin.black.raison.detre@gmail.com>
2022-10-16 01:52:43 +0800

    ✨ feat(group): added table column

--- database/migrations/2022_06_25_124742_create_groups_table.php
+++ database/migrations/2022_06_25_124742_create_groups_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
   public function up() {
     Schema::create('groups', function (Blueprint $table) {
       $table->id();
+      $table->uuid();
 
       $table->string('name', 64);
 
