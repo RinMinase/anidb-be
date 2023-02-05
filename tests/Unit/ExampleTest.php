8c6b4188876e19dd79dc2da0d3a297c148b71457
Rin Minase <rin.black.raison.detre@gmail.com>
2023-02-05 17:26:22 +0800

    🧪 test: removed unused unit tests temporarily

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase {
 
-  public function test_that_true_is_true() {
-    $this->assertTrue(true);
-  }
+  // public function test_that_true_is_true() {
+  //   $this->assertTrue(true);
+  // }
 }
