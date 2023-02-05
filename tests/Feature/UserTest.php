a5708c8c4bb4cdf57a9a5e9aacddc69ace79c5e2
Rin Minase <rin.black.raison.detre@gmail.com>
2023-02-05 17:48:50 +0800

    🧪 test(user): added form validation testing for registration

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -43,4 +43,78 @@ class UserTest extends BaseTestCase {
         ]
       ]);
   }
+
+  public function test_user_registration_with_invalid_password() {
+    $test_email = "invalid email";
+    $test_password = "12345";
+
+    $response = $this->post('/api/auth/register', [
+      'email' => $test_email,
+      'password' => $test_password,
+      'password_confirmation' => $test_password,
+    ]);
+
+    $response->assertStatus(401)
+      ->assertJson([
+        'data' => [
+          'password' => [
+            'The password must be at least 6 characters.'
+          ]
+        ]
+      ]);
+  }
+
+  public function test_user_registration_with_blank_email() {
+    $test_password = "e9597119-8452-4f2b-96d8-f2b1b1d2f158";
+
+    $response = $this->post('/api/auth/register', [
+      'password' => $test_password,
+      'password_confirmation' => $test_password,
+    ]);
+
+    $response->assertStatus(401)
+      ->assertJson([
+        'data' => [
+          'email' => [
+            'The email field is required.'
+          ]
+        ]
+      ]);
+  }
+
+  public function test_user_registration_with_blank_password() {
+    $test_email = "unit_testing@mail.com";
+
+    $response = $this->post('/api/auth/register', [
+      'email' => $test_email,
+    ]);
+
+    $response->assertStatus(401)
+      ->assertJson([
+        'data' => [
+          'password' => [
+            'The password field is required.'
+          ]
+        ]
+      ]);
+  }
+
+  public function test_user_registration_with_invalid_password_confirmation() {
+    $test_email = "unit_testing@mail.com";
+    $test_password = "e9597119-8452-4f2b-96d8-f2b1b1d2f158";
+
+    $response = $this->post('/api/auth/register', [
+      'email' => $test_email,
+      'password' => $test_password,
+    ]);
+
+    $response->assertStatus(401)
+      ->assertJson([
+        'data' => [
+          'password' => [
+            'The password confirmation does not match.'
+          ]
+        ]
+      ]);
+  }
 }
