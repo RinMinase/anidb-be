2baa928f7305765428d0b2ea638d05d093380a0e
Rin Minase <rin.black.raison.detre@gmail.com>
2023-02-09 15:39:15 +0800

    🧪 test(mal): added test cases

---
+++ tests/Feature/MALTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\BaseTestCase;
+
+class MALTest extends BaseTestCase {
+
+  public function test_mal_search() {
+    $search_keyword = "tensei";
+
+    $this->withoutMiddleware();
+
+    $response = $this->get('/api/mal/' . $search_keyword);
+
+    $response->assertStatus(200)
+      ->assertJsonCount(5)
+      ->assertJsonStructure([['id', 'title']]);
+  }
+
+  public function test_mal_get_info() {
+    $search_keyword = "39535";
+
+    $this->withoutMiddleware();
+
+    $response = $this->get('/api/mal/' . $search_keyword);
+
+    $response->assertStatus(200)
+      ->assertJsonStructure([
+        'url',
+        'title',
+        'synonyms',
+        'episodes',
+        'premiered',
+      ]);
+  }
+}
