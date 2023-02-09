6407901c1cf388204b4b73c3b8c56c447ed4d4c2
Rin Minase <rin.black.raison.detre@gmail.com>
2023-02-10 00:45:41 +0800

    🧪 test(mal): simplified functions on fetching

--- tests/Feature/MAL/MALTest.php
+++ tests/Feature/MAL/MALTest.php
@@ -9,9 +9,8 @@ class MALTest extends BaseTestCase {
   public function test_mal_search() {
     $search_keyword = "tensei";
 
-    $this->withoutMiddleware();
-
-    $response = $this->get('/api/mal/' . $search_keyword);
+    $response = $this->withoutMiddleware()
+      ->get('/api/mal/' . $search_keyword);
 
     $response->assertStatus(200)
       ->assertJsonCount(5)
@@ -21,9 +20,8 @@ class MALTest extends BaseTestCase {
   public function test_mal_get_info() {
     $search_keyword = "39535";
 
-    $this->withoutMiddleware();
-
-    $response = $this->get('/api/mal/' . $search_keyword);
+    $response = $this->withoutMiddleware()
+      ->get('/api/mal/' . $search_keyword);
 
     $response->assertStatus(200)
       ->assertJsonStructure([
