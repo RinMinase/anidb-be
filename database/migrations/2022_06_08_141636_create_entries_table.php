59e3bad4f558ee682f371f17ec7af24efff71443
Rin Minase <rin.black.raison.detre@gmail.com>
2023-02-11 00:54:43 +0800

    🐛 fix(entry): modified id column to unique

--- database/migrations/2022_06_08_141636_create_entries_table.php
+++ database/migrations/2022_06_08_141636_create_entries_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
   public function up() {
     Schema::create('entries', function (Blueprint $table) {
       $table->id();
-      $table->uuid('uuid');
+      $table->uuid('uuid')->unique();
 
       $table->integer('id_quality')->nullable();
       $table->foreign('id_quality')->references('id')->on('qualities');
