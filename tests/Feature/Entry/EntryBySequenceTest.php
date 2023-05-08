9b0dcd243a770d83a5ba0e12b5a25bb6121b284a
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-09 00:36:38 +0800

    🧪 test(entry): added test cases for entry by sequence queries

--- tests/Feature/Entry/EntryBySequenceTest.php
+++ tests/Feature/Entry/EntryBySequenceTest.php
@@ -3,80 +3,40 @@
 namespace Tests\Feature\Entry;
 
 use Tests\BaseTestCase;
-use Illuminate\Support\Str;
-use Carbon\Carbon;
-use Faker\Factory as Faker;
-
-use App\Models\Entry;
 
 class EntryBySequenceTest extends BaseTestCase {
 
-  private $entry_uuid = "e9597119-8452-4f2b-96d8-f2b1b1d2f158";
+  public function test_get_entries_by_sequence() {
+    $response = $this->withoutMiddleware()
+      ->get('/api/entries/by-sequence/1');
 
-  private function setup_config() {
-    $faker = Faker::create();
-
-    // Clearing possible duplicate data
-    $this->setup_clear();
-
-    Entry::create([
-      'uuid' => $this->entry_uuid,
-      'id_quality' => 1,
-      'title' => "title",
-      'date_finished' => "2001-01-01",
-      'duration' => 10_000,
-      'filesize' => 10_000_000,
-      'episodes' => 30,
-      'ovas' => 20,
-      'specials' => 10,
-      'encoder_video' => "encoder video",
-      'encoder_audio' => "encoder audio",
-      'encoder_subs' => "encoder subs",
-      'codec_hdr' => 1,
-      'codec_video' => 1,
-      'codec_audio' => 1,
-      'release_year' => 2000,
-      'release_season' => "Winter",
-      'variants' => "variant",
-      'remarks' => "remark",
-    ]);
-
-    $uuid_list = [];
-    $additional_test_data = [];
-
-    for ($i = 0; $i < 40; $i++) {
-      $new_uuid = Str::uuid()->toString();
-
-      while (in_array($new_uuid, $uuid_list)) {
-        $new_uuid = Str::uuid()->toString();
-      }
-
-      $uuid_list[] = $new_uuid;
-      $additional_test_data[] = [
-        'uuid' => $new_uuid,
-        'id_quality' => 1,
-        'title' => 'test data --- ' . $faker->text(20),
-        'created_at' => Carbon::now(),
-        'updated_at' => Carbon::now(),
-      ];
-    }
-
-    Entry::insert($additional_test_data);
+    $response->assertStatus(200)
+      ->assertJsonStructure([
+        'data' => [
+          'data',
+          'stats' => [
+            'titles_per_day',
+            'eps_per_day',
+            'quality_2160',
+            'quality_1080',
+            'quality_720',
+            'quality_480',
+            'quality_360',
+            'total_titles',
+            'total_eps',
+            'total_size',
+            'total_days',
+            'start_date',
+            'end_date',
+          ],
+        ],
+      ]);
   }
 
-  private function setup_clear() {
-    Entry::where('uuid', $this->entry_uuid)->forceDelete();
+  public function test_get_entries_by_sequence_no_auth() {
+    $response = $this->get('/api/entries/by-sequence/1');
 
-    $entries = Entry::where('title', 'LIKE', 'test data --- %')->get();
-
-    foreach ($entries as $entry) {
-      if ($entry->season_first_title_id) $entry->season_first_title()->dissociate();
-      if ($entry->prequel_id) $entry->prequel()->dissociate();
-      if ($entry->sequel_id) $entry->sequel()->dissociate();
-
-      $entry->save();
-    }
-
-    $entries->map(fn ($entry) => $entry->forceDelete());
+    $response->assertStatus(401)
+      ->assertJson(['message' => 'Unauthorized']);
   }
 }
