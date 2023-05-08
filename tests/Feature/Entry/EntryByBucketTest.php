3c12b8456431895222e9add1a63e47ba26c7acb5
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-09 00:08:00 +0800

    🧪 test(entry): added tests for bucket queries

--- tests/Feature/Entry/EntryByBucketTest.php
+++ tests/Feature/Entry/EntryByBucketTest.php
@@ -8,38 +8,15 @@ use Carbon\Carbon;
 use Faker\Factory as Faker;
 
 use App\Models\Entry;
+use App\Models\Bucket;
 
 class EntryByBucketTest extends BaseTestCase {
-
-  private $entry_uuid = "e9597119-8452-4f2b-96d8-f2b1b1d2f158";
 
   private function setup_config() {
     $faker = Faker::create();
 
     // Clearing possible duplicate data
     $this->setup_clear();
-
-    Entry::create([
-      'uuid' => $this->entry_uuid,
-      'id_quality' => 1,
-      'title' => "title",
-      'date_finished' => "2001-01-01",
-      'duration' => 10_000,
-      'filesize' => 10_000_000,
-      'episodes' => 30,
-      'ovas' => 20,
-      'specials' => 10,
-      'encoder_video' => "encoder video",
-      'encoder_audio' => "encoder audio",
-      'encoder_subs' => "encoder subs",
-      'codec_hdr' => 1,
-      'codec_video' => 1,
-      'codec_audio' => 1,
-      'release_year' => 2000,
-      'release_season' => "Winter",
-      'variants' => "variant",
-      'remarks' => "remark",
-    ]);
 
     $uuid_list = [];
     $additional_test_data = [];
@@ -65,18 +42,77 @@ class EntryByBucketTest extends BaseTestCase {
   }
 
   private function setup_clear() {
-    Entry::where('uuid', $this->entry_uuid)->forceDelete();
+    Entry::where('title', 'LIKE', 'test data --- %')->forceDelete();
+  }
 
-    $entries = Entry::where('title', 'LIKE', 'test data --- %')->get();
+  public function test_get_buckets_with_entry_stats() {
+    $this->setup_config();
 
-    foreach ($entries as $entry) {
-      if ($entry->season_first_title_id) $entry->season_first_title()->dissociate();
-      if ($entry->prequel_id) $entry->prequel()->dissociate();
-      if ($entry->sequel_id) $entry->sequel()->dissociate();
+    $buckets = Bucket::select('id', 'from', 'to')->get();
 
-      $entry->save();
+    $test_data_start = 't';
+    $bucket_id = null;
+
+    foreach ($buckets as $bucket) {
+      if ($test_data_start >= $bucket->from && $test_data_start <= $bucket->to) {
+        $bucket_id = $bucket->id;
+      }
     }
 
-    $entries->map(fn ($entry) => $entry->forceDelete());
+    $this->assertNotNull($bucket_id, 'No Bucket was found to match the letter "T"');
+
+    if ($bucket_id) {
+      $response = $this->withoutMiddleware()
+        ->get('/api/entries/by-bucket/' . $bucket_id);
+
+      $response->assertStatus(200)
+        ->assertJsonCount(40, 'data.data')
+        ->assertJsonStructure([
+          'data' => [
+            'data' => [[]],
+            'stats' => [
+              'from',
+              'to',
+            ],
+          ],
+        ]);
+    }
+
+    $this->setup_clear();
+  }
+
+  public function test_get_buckets_with_entry_stats_no_auth() {
+    $response = $this->get('/api/entries/by-bucket/');
+
+    $response->assertStatus(401)
+      ->assertJson(['message' => 'Unauthorized']);
+  }
+
+  public function test_get_entries_by_bucket() {
+    $response = $this->withoutMiddleware()
+      ->get('/api/entries/by-bucket/');
+
+    $response->assertStatus(200)
+      ->assertJsonStructure([
+        'data' => [[
+          'id',
+          'from',
+          'to',
+          'free',
+          'freeTB',
+          'used',
+          'percent',
+          'total',
+          'rawTotal',
+          'titles',
+        ]],
+      ]);
+  }
+
+  public function test_get_entries_by_bucket_no_auth() {
+    $response = $this->get('/api/entries/by-bucket/1');
+
+    $response->assertStatus(401)
+      ->assertJson(['message' => 'Unauthorized']);
   }
 }
