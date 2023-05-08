ff6084f0c2f85b0727d855668db033bb461699d9
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-08 22:57:34 +0800

    ✨ feat(entry): added cascade delete on ratings

--- database/migrations/2022_06_09_141551_create_entries_rating_table.php
+++ database/migrations/2022_06_09_141551_create_entries_rating_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
       $table->id();
 
       $table->integer('id_entries')->unsigned()->nullable();
-      $table->foreign('id_entries')->references('id')->on('entries');
+      $table->foreign('id_entries')->references('id')->on('entries')->onDelete('cascade');
 
       $table->tinyInteger('audio')->unsigned()->default(0)->nullable();
       $table->tinyInteger('enjoyment')->unsigned()->default(0)->nullable();
