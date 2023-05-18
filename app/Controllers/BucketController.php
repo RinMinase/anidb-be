cf1458ef375e3a60cb2bc9619e420c7baaf8c2db
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-19 06:44:42 +0800

    📝 docs(bucket): removed unused api docs

--- app/Controllers/BucketController.php
+++ app/Controllers/BucketController.php
@@ -19,37 +19,6 @@ class BucketController extends Controller {
   }
 
   /**
-   * @api {get} /api/buckets Retrieve all buckets
-   * @apiName BucketRetrieve
-   * @apiGroup Bucket
-   *
-   * @apiHeader {String} Authorization Token received from logging-in
-   *
-   * @apiSuccess {Object[]} data Bucket Data
-   * @apiSuccess {Number} data.id ID of the library
-   * @apiSuccess {String} data.from Starting letter of library
-   * @apiSuccess {String} data.to Ending letter of library
-   * @apiSuccess {Number} data.size Size of the library
-   * @apiSuccess {DateTime} data.created_at Creation date of the library
-   *
-   * @apiSuccessExample Success Response
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "data": [
-   *         {
-   *           id: 1,
-   *           from: "a",
-   *           to: "d",
-   *           size: 2000339066880,
-   *           created_at: "2020-01-01 00:00:00",
-   *         }
-   *       ]
-   *     }
-   *
-   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
-   */
-
-  /**
    * @OA\Get(
    *   tags={"Bucket"},
    *   path="/api/buckets",
