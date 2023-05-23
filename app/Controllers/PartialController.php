7b93978ec7b20e107c39f966520d95517391a6fd
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-23 11:08:15 +0800

    📝 docs(catalog): added api docs

--- app/Controllers/PartialController.php
+++ app/Controllers/PartialController.php
@@ -82,6 +82,39 @@ class PartialController extends Controller {
     }
   }
 
+  /**
+   * @OA\Post(
+   *   tags={"Catalog"},
+   *   path="/api/partials/multi",
+   *   summary="Multi Add a Partial Entry",
+   *   security={{"token":{}}},
+   *
+   *   @OA\Parameter(
+   *     name="data",
+   *     in="query",
+   *     required=true,
+   *     example="low[0]=Title Low 1&normal[0]=Title Normal 1&normal[1]=Title Normal 2&high[0]=Title High 1",
+   *     @OA\Schema(type="string"),
+   *   ),
+   *   @OA\Parameter(
+   *     name="season",
+   *     in="query",
+   *     required=true,
+   *     example="Winter",
+   *     @OA\Schema(type="string", enum={"Winter", "Spring", "Summer", "Fall"}),
+   *   ),
+   *   @OA\Parameter(
+   *     name="year",
+   *     in="query",
+   *     required=true,
+   *     example=2021,
+   *     @OA\Schema(type="integer", format="int32"),
+   *   ),
+   *
+   *   @OA\Response(response=200, ref="#/components/responses/Success"),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   * )
+   */
   public function add_multiple(Request $request): JsonResponse {
     try {
       $data = [];
@@ -182,6 +215,47 @@ class PartialController extends Controller {
     }
   }
 
+  /**
+   * @OA\Put(
+   *   tags={"Catalog"},
+   *   path="/api/partials/multi/{catalog_id}",
+   *   summary="Multi Add a Partial Entry",
+   *   security={{"token":{}}},
+   *
+   *   @OA\Parameter(
+   *     name="catalog_id",
+   *     in="path",
+   *     required=true,
+   *     example="e9597119-8452-4f2b-96d8-f2b1b1d2f158",
+   *     description="Catalog ID",
+   *     @OA\Schema(type="string", format="uuid"),
+   *   ),
+   *   @OA\Parameter(
+   *     name="data",
+   *     in="query",
+   *     required=true,
+   *     example="low[0]=Title Low 1&normal[0]=Title Normal 1&normal[1]=Title Normal 2&high[0]=Title High 1",
+   *     @OA\Schema(type="string"),
+   *   ),
+   *   @OA\Parameter(
+   *     name="season",
+   *     in="query",
+   *     required=true,
+   *     example="Winter",
+   *     @OA\Schema(type="string", enum={"Winter", "Spring", "Summer", "Fall"}),
+   *   ),
+   *   @OA\Parameter(
+   *     name="year",
+   *     in="query",
+   *     required=true,
+   *     example=2021,
+   *     @OA\Schema(type="integer", format="int32"),
+   *   ),
+   *
+   *   @OA\Response(response=200, ref="#/components/responses/Success"),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   * )
+   */
   public function edit_multiple(Request $request, $uuid): JsonResponse {
     try {
       $data = [];
