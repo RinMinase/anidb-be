ac659be8b07bc18f04158441dbfdee2fb2525970
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-23 11:41:35 +0800

    📝 docs(entry): added api docs for last api

--- app/Controllers/EntryLastController.php
+++ app/Controllers/EntryLastController.php
@@ -15,53 +15,50 @@ class EntryLastController extends Controller {
     $this->entryRepository = $entryRepository;
   }
 
-
   /**
-   * @api {get} /api/entries/last Retrieve Latest Entries
-   * @apiName RetrieveLatestEntry
-   * @apiGroup Entry
-   *
-   * @apiHeader {String} token User login token
-   *
-   * @apiSuccess {Object[]} data Entry data
-   * @apiSuccess {UUID} data.id Entry ID
-   * @apiSuccess {Date} data.dateFinished Date fisished or date last rewatched
-   * @apiSuccess {String} data.encoder Title encoder
-   * @apiSuccess {Number} data.episodes Number of episodes
-   * @apiSuccess {String} data.filesize Filesize in nearest byte unit
-   * @apiSuccess {Number} data.ovas Number of OVAs
-   * @apiSuccess {String='4K 2160p','FHD 1080p','HD 720p','HQ 480p','LQ 360p'} data.quality Video quality
-   * @apiSuccess {Number} data.rating Averaged rating of Audio, Enjoyment, Graphics and Plot
-   * @apiSuccess {String} data.release Season and year in which the title was released
-   * @apiSuccess {String} data.remarks Any remarks for the title
-   * @apiSuccess {Boolean} data.rewatched Flag to check if date stated is alread rewatched date
-   * @apiSuccess {Number} data.specials Number of specials
-   * @apiSuccess {String} data.title Entry title
-   *
-   * @apiSuccessExample Success Response
-   *     HTTP/1.1 200 OK
-   *     [
-   *       {
-   *         "id": "9ef81943-78f0-4d1c-a831-a59fb5af339c"
-   *         "dateFinished": "Mar 01, 2011",
-   *         "encoder": "encoder—encoder2",
-   *         "episodes": 25,
-   *         "filesize": "10.25 GB",
-   *         "ovas": 1,
-   *         "quality": "FHD 1080p",
-   *         "rating": 7.5,
-   *         "release": "Spring 2017",
-   *         "remarks": "some remarks",
-   *         "specials": 1,
-   *         "title": "Title"
-   *       }, { ... }
-   *     ]
-   *
-   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   * @OA\Get(
+   *   tags={"Entry"},
+   *   path="/api/entries/last",
+   *   summary="Get Latest Entries",
+   *   security={{"token":{}}},
+   *   @OA\Response(
+   *     response=200,
+   *     description="OK",
+   *     @OA\JsonContent(
+   *       @OA\Property(property="data", ref="#/components/schemas/EntryCollection"),
+   *       @OA\Property(
+   *         example={
+   *           "dateLastEntry": "Apr 01, 2015",
+   *           "daysLastEntry": 2974,
+   *           "dateOldestEntry": "Jan 01, 2011",
+   *           "daysOldestEntry": 4525,
+   *           "totalEps": 0,
+   *           "totalTitles": 7,
+   *           "totalCours": 0,
+   *           "titlesPerWeek": 0.01,
+   *           "coursPerWeek": 0,
+   *           "epsPerWeek": 0,
+   *           "epsPerDay": 0,
+   *         },
+   *         property="stats",
+   *         type="object",
+   *         @OA\Property(property="dateLastEntry", type="string"),
+   *         @OA\Property(property="daysLastEntry", type="integer", format="int32"),
+   *         @OA\Property(property="dateOldestEntry", type="string"),
+   *         @OA\Property(property="daysOldestEntry", type="integer", format="int32"),
+   *         @OA\Property(property="totalEps", type="integer", format="int32"),
+   *         @OA\Property(property="totalTitles", type="integer", format="int32"),
+   *         @OA\Property(property="titlesPerWeek", type="number"),
+   *         @OA\Property(property="coursPerWeek", type="integer", format="int32"),
+   *         @OA\Property(property="epsPerWeek", type="integer", format="int32"),
+   *         @OA\Property(property="epsPerDay", type="integer", format="int32"),
+   *       ),
+   *     )
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   * )
    */
   public function index(): JsonResponse {
-    return response()->json([
-      'data' => $this->entryRepository->getLast(),
-    ]);
+    return response()->json($this->entryRepository->getLast());
   }
 }
