724fac8ca3820fdb7bf9792a1d5be10a8b26b983
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-25 02:09:40 +0800

    📝 docs(entry): added api docs

--- app/Controllers/EntryByYearController.php
+++ app/Controllers/EntryByYearController.php
@@ -14,51 +14,65 @@ class EntryByYearController extends Controller {
     $this->entryRepository = $entryRepository;
   }
 
-
   /**
-   * @api {get} /api/entries/by-year Retrieve By Year Stats
-   * @apiName RetrieveByYearStats
-   * @apiGroup Entry
-   *
-   * @apiHeader {String} token User login token
-   *
-   * @apiSuccess {Object[]} data By Year Data
-   * @apiSuccess {String} data.year Year the count belongs to
-   * @apiSuccess {Object} data.seasons Year the count belongs to
-   * @apiSuccess {Number} data.seasons.None No-season entries in the year
-   * @apiSuccess {Number} data.seasons.Winter Winter entries in the year
-   * @apiSuccess {Number} data.seasons.Spring Spring entries in the year
-   * @apiSuccess {Number} data.seasons.Summer Summer entries in the year
-   * @apiSuccess {Number} data.seasons.Fall Fall entries in the year
-   * @apiSuccess {Number} data.count Only present in first NO-year ANY-season item
-   *
-   * @apiSuccessExample Success Response
-   *     HTTP/1.1 200 OK
-   *     [
-   *       {
-   *         "year": null,
-   *         "seasons": null,
-   *         "count": 2
-   *       },
-   *       {
-   *         "year": 2010,
-   *         "seasons": {
-   *           Spring: 1,
-   *           Summer: 2,
-   *         },
-   *         "count": null
-   *       },
-   *       {
-   *         "year": 2020,
-   *         "seasons": {
-   *           None: 1,
-   *           Winter: 2,
-   *         },
-   *         "count": null
-   *       },
-   *     ]
-   *
-   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   * @OA\Get(
+   *   tags={"Entry Specific"},
+   *   path="/api/entries/by-year",
+   *   summary="Get All By Year Stats with Entries",
+   *   security={{"token":{}}},
+   *   @OA\Response(
+   *     response=200,
+   *     description="Success",
+   *     @OA\JsonContent(
+   *       @OA\Property(
+   *         example={{
+   *           "year": null,
+   *           "seasons": null,
+   *           "count": 2
+   *         }, {
+   *           "year": 2010,
+   *           "seasons": {
+   *             "Winter": 1,
+   *             "Spring": 1,
+   *             "Summer": 2,
+   *             "Fall": 2,
+   *           },
+   *           "count": null
+   *         }},
+   *         property="data",
+   *         type="array",
+   *         @OA\Items(
+   *           @OA\Property(
+   *             property="year",
+   *             type="integer",
+   *             format="int32",
+   *             nullable=true,
+   *             description="null value on uncategorized entries",
+   *           ),
+   *           @OA\Property(
+   *             property="seasons",
+   *             type="object",
+   *             nullable=true,
+   *             description="null value on uncategorized entries",
+   *             @OA\Property(property="Winter", type="integer", format="int32"),
+   *             @OA\Property(property="Spring", type="integer", format="int32"),
+   *             @OA\Property(property="Summer", type="integer", format="int32"),
+   *             @OA\Property(property="Fall", type="integer", format="int32"),
+   *           ),
+   *           @OA\Property(
+   *             property="count",
+   *             type="integer",
+   *             format="int32",
+   *             nullable=true,
+   *             description="null value whenever seasons is present",
+   *           ),
+   *         ),
+   *       ),
+   *     ),
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=500, ref="#/components/responses/Failed"),
+   * )
    */
   public function index(): JsonResponse {
     return response()->json([
@@ -66,36 +80,42 @@ class EntryByYearController extends Controller {
     ]);
   }
 
-
   /**
-   * @api {get} /api/entries/by-year/:year Retrieve By Season Entries
-   * @apiName RetrieveBySeason
-   * @apiGroup Entry
+   * @OA\Get(
+   *   tags={"Entry Specific"},
+   *   path="/api/entries/by-year/{year}",
+   *   summary="Get All By Year Stats with Entries",
+   *   security={{"token":{}}},
    *
-   * @apiHeader {String} token User login token
-   * @apiParam {Number|String} year Release year, if String value is passed year is considered null
+   *   @OA\Parameter(
+   *     name="year",
+   *     description="If year is not valid or null is passed, it fetches for uncategorized entries",
+   *     in="path",
+   *     example=2000,
+   *     @OA\Schema(type="integer", format="int32", minimum=1970, maximum=2999),
+   *   ),
    *
-   * @apiSuccess {Object} data By season data
-   * @apiSuccess {Object} data.season Entry data per season
-   * @apiSuccess {UUID} data.season.id Entry ID
-   * @apiSuccess {String} data.season.title Entry title
-   *
-   * @apiSuccessExample Success Response
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "Winter": [
-   *         {
-   *           "id": "9ef81943-78f0-4d1c-a831-a59fb5af339c"
-   *           "title": "Title"
-   *         }, { ... }
-   *       ],
-   *       "Spring": [],
-   *       "Summer": [],
-   *       "Fall": [],
-   *       "Uncategorized": [],
-   *     }
-   *
-   * @apiError Unauthorized There is no login token provided, or the login token provided is invalid
+   *   @OA\Response(
+   *     response=200,
+   *     description="Success",
+   *     @OA\JsonContent(
+   *       @OA\Property(
+   *         property="data",
+   *         type="object",
+   *         @OA\Property(property="Winter", ref="#/components/schemas/EntryCollection"),
+   *         @OA\Property(property="Spring", ref="#/components/schemas/EntryCollection"),
+   *         @OA\Property(property="Summer", ref="#/components/schemas/EntryCollection"),
+   *         @OA\Property(property="Fall", ref="#/components/schemas/EntryCollection"),
+   *         @OA\Property(
+   *           property="Uncategorized",
+   *           ref="#/components/schemas/EntryCollection",
+   *         ),
+   *       ),
+   *     ),
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=500, ref="#/components/responses/Failed"),
+   * )
    */
   public function get($year): JsonResponse {
     return response()->json([
