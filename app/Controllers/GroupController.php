b450013a832cde9de79158eae5c294901d7d5aa4
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-27 10:17:10 +0800

    📝 docs(group): added lacking responses

--- app/Controllers/GroupController.php
+++ app/Controllers/GroupController.php
@@ -126,6 +126,7 @@ class GroupController extends Controller {
    *
    *   @OA\Response(response=200, ref="#/components/responses/Success"),
    *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=404, ref="#/components/responses/NotFound"),
    * )
    */
   public function edit(Request $request, $uuid): JsonResponse {
