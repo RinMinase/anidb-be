d14e6f39c5d240449fbfbb22645caa4a6111393f
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-28 02:09:12 +0800

    ✨ feat(import): modified to use generic responses

--- app/Controllers/ImportController.php
+++ app/Controllers/ImportController.php
@@ -81,57 +81,50 @@ class ImportController extends Controller {
    * )
    */
   public function index(Request $request): JsonResponse {
-    try {
-      $file = json_decode($request->file('file')->get());
-      $import_count = $this->importRepository->import($file);
+    $file = json_decode($request->file('file')->get());
+    $import_count = $this->importRepository->import($file);
 
-      $entry_count = 0;
-      if (!empty($file->entry)) {
-        $entry_count = count($file->entry);
-      }
-
-      $bucket_count = 0;
-      if (!empty($file->bucket)) {
-        $bucket_count = count($file->bucket);
-      }
-
-      $sequence_count = 0;
-      if (!empty($file->sequence)) {
-        $sequence_count = count($file->sequence);
-      }
-
-      $group_count = 0;
-      if (!empty($file->group)) {
-        $group_count = count($file->group);
-      }
-
-      return response()->json([
-        'status' => 200,
-        'message' => 'Success',
-        'data' => [
-          'entries' => [
-            'accepted' => $import_count['entry'],
-            'total' => $entry_count,
-          ],
-          'buckets' => [
-            'accepted' => $import_count['bucket'],
-            'total' => $bucket_count,
-          ],
-          'sequences' => [
-            'accepted' => $import_count['sequence'],
-            'total' => $sequence_count,
-          ],
-          'groups' => [
-            'accepted' => $import_count['group'],
-            'total' => $group_count,
-          ],
-        ]
-      ]);
-    } catch (Exception) {
-      return response()->json([
-        'status' => 500,
-        'message' => 'Failed',
-      ], 500);
+    $entry_count = 0;
+    if (!empty($file->entry)) {
+      $entry_count = count($file->entry);
     }
+
+    $bucket_count = 0;
+    if (!empty($file->bucket)) {
+      $bucket_count = count($file->bucket);
+    }
+
+    $sequence_count = 0;
+    if (!empty($file->sequence)) {
+      $sequence_count = count($file->sequence);
+    }
+
+    $group_count = 0;
+    if (!empty($file->group)) {
+      $group_count = count($file->group);
+    }
+
+    return response()->json([
+      'status' => 200,
+      'message' => 'Success',
+      'data' => [
+        'entries' => [
+          'accepted' => $import_count['entry'],
+          'total' => $entry_count,
+        ],
+        'buckets' => [
+          'accepted' => $import_count['bucket'],
+          'total' => $bucket_count,
+        ],
+        'sequences' => [
+          'accepted' => $import_count['sequence'],
+          'total' => $sequence_count,
+        ],
+        'groups' => [
+          'accepted' => $import_count['group'],
+          'total' => $group_count,
+        ],
+      ]
+    ]);
   }
 }
