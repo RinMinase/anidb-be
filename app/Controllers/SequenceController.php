a18a76d04af35b252aae0d358f5d105aca2478cd
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-28 00:31:57 +0800

    📝 docs(sequence): added api docs for import

--- app/Controllers/SequenceController.php
+++ app/Controllers/SequenceController.php
@@ -191,6 +191,49 @@ class SequenceController extends Controller {
     return DefaultResponse::success();
   }
 
+  /**
+   * @OA\Post(
+   *   tags={"Import"},
+   *   path="/api/sequences/import",
+   *   summary="Import a JSON file to seed data for sequences table",
+   *   security={{"token":{}}},
+   *
+   *   @OA\RequestBody(
+   *     required=true,
+   *     @OA\MediaType(
+   *       mediaType="multipart/form-data",
+   *       @OA\Schema(
+   *         type="object",
+   *         @OA\Property(property="file", type="string", format="binary"),
+   *       ),
+   *     ),
+   *   ),
+   *
+   *   @OA\Response(
+   *     response=200,
+   *     description="Success",
+   *     @OA\JsonContent(
+   *       example={
+   *         "status": 200,
+   *         "message": "Success",
+   *         "data": {
+   *           "acceptedImports": 0,
+   *           "totalJsonEntries": 0,
+   *         },
+   *       },
+   *       @OA\Property(property="status", type="integer", format="int32"),
+   *       @OA\Property(property="message", type="integer", format="int32"),
+   *       @OA\Property(
+   *         property="data",
+   *         @OA\Property(property="acceptedImports", type="integer", format="int32"),
+   *         @OA\Property(property="totalJsonEntries", type="integer", format="int32"),
+   *       ),
+   *     ),
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=500, ref="#/components/responses/Failed"),
+   * )
+   */
   public function import(Request $request) {
     try {
       $count = $this->sequenceRepository->import($request->all());
