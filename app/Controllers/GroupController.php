1b7eb3bf8e4309d65b0fef2369dd74e7f5fc56d4
Rin Minase <rin.black.raison.detre@gmail.com>
2023-05-28 00:33:06 +0800

    📝 docs(group): added api docs for import

--- app/Controllers/GroupController.php
+++ app/Controllers/GroupController.php
@@ -184,6 +184,49 @@ class GroupController extends Controller {
     }
   }
 
+  /**
+   * @OA\Post(
+   *   tags={"Import"},
+   *   path="/api/groups/import",
+   *   summary="Import a JSON file to seed data for groups table",
+   *   security={{"token":{}}},
+   *
+   *   @OA\RequestBody(
+   *     required=true,
+   *     @OA\MediaType(
+   *       mediaType="multipart/form-data",
+   *       @OA\Schema(
+   *         type="object",
+   *         @OA\Property(property="file", type="string", format="binary"),
+   *       ),
+   *     ),
+   *   ),
+   *
+   *   @OA\Response(
+   *     response=200,
+   *     description="Success",
+   *     @OA\JsonContent(
+   *       example={
+   *         "status": 200,
+   *         "message": "Success",
+   *         "data": {
+   *           "acceptedImports": 0,
+   *           "totalJsonEntries": 0,
+   *         },
+   *       },
+   *       @OA\Property(property="status", type="integer", format="int32"),
+   *       @OA\Property(property="message", type="integer", format="int32"),
+   *       @OA\Property(
+   *         property="data",
+   *         @OA\Property(property="acceptedImports", type="integer", format="int32"),
+   *         @OA\Property(property="totalJsonEntries", type="integer", format="int32"),
+   *       ),
+   *     ),
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=500, ref="#/components/responses/Failed"),
+   * )
+   */
   public function import(Request $request) {
     try {
       $file = json_decode($request->file('file')->get());
