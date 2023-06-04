36e435c90f9bca884e971813ca3e2269dbf5d0f2
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-05 01:10:37 +0800

    📝 docs: added request duration in api docs

--- app/docs.blade.php
+++ app/docs.blade.php
@@ -189,6 +189,7 @@
         filter: {!! config('l5-swagger.defaults.ui.display.filter') ? 'true' : 'false' !!},
         persistAuthorization: "{!! config('l5-swagger.defaults.ui.authorization.persist_authorization') ? 'true' : 'false' !!}",
         defaultModelsExpandDepth: -1,
+        displayRequestDuration: true,
       })
 
       window.ui = ui
