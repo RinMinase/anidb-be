6a67df10d11218f9cc7f84c51b4593f1a4c714d1
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-08 03:25:19 +0800

    🐛 fix: fixed validation rules

--- app/Rules/SignedSmallIntRule.php
+++ app/Rules/SignedSmallIntRule.php
@@ -9,7 +9,7 @@ class SignedSmallIntRule implements ValidationRule {
 
   public function validate(string $attribute, mixed $value, Closure $fail): void {
     if ($value > 32767 || $value < -32767) {
-      $fail($attribute . ' must not be greater than 127 or -127');
+      $fail($attribute . ' must not be greater than 32767 or -32767');
     }
   }
 }
