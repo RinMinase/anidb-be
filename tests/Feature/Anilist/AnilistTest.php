be3dbba7ce2c1637989e2bcae2bf9d18316f97d1
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-08 00:22:26 +0800

    🧪 test(anilist): added test cases

---
+++ tests/Feature/Anilist/AnilistTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Anilist;
+
+use Tests\BaseTestCase;
+
+class AnilistTest extends BaseTestCase {
+
+  public function test_anilist_search() {
+    $search_keyword = "ten";
+
+    $response = $this->withoutMiddleware()
+      ->get('/api/anilist/search?query=' . $search_keyword);
+
+    $response->assertStatus(200)
+      ->assertJsonCount(15, 'data')
+      ->assertJsonStructure([
+        'data' => [[
+          'id',
+          'title',
+        ]]
+      ]);
+  }
+
+  public function test_anilist_search_no_auth() {
+    $search_keyword = "ten";
+
+    $response = $this->get('/api/anilist/search?query=' . $search_keyword);
+
+    $response->assertStatus(401)
+      ->assertJson(['message' => 'Unauthorized']);
+  }
+
+  public function test_anilist_get_title() {
+    $id = "101280";
+
+    $response = $this->withoutMiddleware()
+      ->get('/api/anilist/title/' . $id);
+
+    $response->assertStatus(200)
+      ->assertJsonStructure([
+        'data' => [
+          'url',
+          'title',
+          'episodes',
+          'premiered',
+        ]
+      ]);
+  }
+
+  public function test_anilist_get_title_no_auth() {
+    $id = "101280";
+
+    $response = $this->get('/api/anilist/title/' . $id);
+
+    $response->assertStatus(401)
+      ->assertJson(['message' => 'Unauthorized']);
+  }
+}
