74e834e0d8dc45aeb40cfe2f88052838014202fa
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-09 00:49:50 +0800

    ✨ feat: improved default response formatting

--- app/Resources/DefaultResponse.php
+++ app/Resources/DefaultResponse.php
@@ -13,12 +13,21 @@ class DefaultResponse {
    *     @OA\Property(property="status", type="integer", format="int32"),
    *     @OA\Property(property="message", type="string"),
    *   ),
+   * ),
+   * @OA\Schema(
+   *   schema="DefaultSuccess",
+   *   @OA\Property(property="status", type="integer", format="int32", example=200),
+   *   @OA\Property(property="message", type="string", example="Success"),
    * )
    */
-  public static function success($message = null) {
-    return response()->json([
+  public static function success(string $message = null, array $data = null) {
+    $defaultResponse = [
       'status' => 200,
       'message' => $message ?? 'Success',
-    ]);
+    ];
+
+    $response = array_merge_recursive($defaultResponse, $data);
+
+    return response()->json($response);
   }
 }
