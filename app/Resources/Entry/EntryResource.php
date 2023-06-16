b743d989f57f244764b405f71108ad59b855ce09
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-17 00:18:12 +0800

    ✨ feat(entry): modified api doc formatting

--- app/Resources/Entry/EntryResource.php
+++ app/Resources/Entry/EntryResource.php
@@ -3,149 +3,125 @@
 namespace App\Resources\Entry;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+
 use Carbon\Carbon;
 use Carbon\CarbonInterval;
 
 /**
  * @OA\Schema(
- *   schema="Entry",
- *   example={
- *     "id": "e9597119-8452-4f2b-96d8-f2b1b1d2f158",
- *     "quality": "4K 2160p",
- *     "title": "Sample Title",
- *     "dateInitFinishedRaw": "2001-01-01T00:00:00.000000Z",
- *     "dateInitFinished": "Jan 01, 2001",
- *     "dateLastFinished": "Mar 01, 2011",
- *     "durationRaw": 10000,
- *     "duration": "2 hours 46 minutes 40 seconds",
- *     "filesizeRaw": 21331439861,
- *     "filesize": "19.87 GB",
- *
- *     "episodes": 25,
- *     "ovas": 1,
- *     "specials": 1,
- *
- *     "seasonNumber": 1,
- *     "seasonFirstTitle": "Sample Title",
- *
- *     "prequelTitle": "Prequel Title",
- *     "prequel": {
- *       "id": "9b712dbe-ff81-41d4-b027-ee87cea4da99",
- *       "title": "Prequel Title",
- *     },
- *     "sequelTitle": "Sequel Title",
- *     "sequel": {
- *       "id": "08d0f87f-6432-47bc-b6bd-c856abbad90f",
- *       "title": "Sequel Title",
- *     },
- *
- *     "encoder": "encoder-encoder2-encoder3",
- *     "encoderVideo": "encoder",
- *     "encoderAudio": "encoder2",
- *     "encoderSubs": "encoder3",
- *
- *     "releaseSeason": "Winter",
- *     "releaseYear": 2000,
- *     "release": "Winter 2000",
- *
- *     "variants": "Variant title",
- *     "remarks": "Some remarks",
- *
- *     "codecHDR": 1,
- *     "id_codec_video": 1,
- *     "codecVideo": "x264 8bit",
- *     "id_codec_audio": 4,
- *     "codecAudio": "FLAC 7.1",
- *
- *     "offquels": {{
- *       "id": "89e3be00-9d4f-4c4f-a99f-c12cbfba04ab",
- *       "title": "Offquel Title"
- *     }},
- *     "rewatches": {{
- *       "id": "af846c35-a51e-4534-9559-c75114e61d84",
- *       "dateIso": "2011-03-01T00:00:00.000000Z",
- *       "date": "March 01, 2011",
- *     }},
- *
- *     "ratingAverage": 4.5,
- *     "rating": {
- *       "audio": 6,
- *       "enjoyment": 5,
- *       "graphics": 4,
- *       "plot": 3
- *     },
- *     "image": "{{ image url }}",
- *    },
- *   @OA\Property(property="id", type="string", format="uuid"),
+ *   @OA\Property(
+ *     property="id",
+ *     type="string",
+ *     format="uuid",
+ *     example="e9597119-8452-4f2b-96d8-f2b1b1d2f158",
+ *   ),
  *   @OA\Property(
  *     property="quality",
  *     type="string",
- *     enum={"4K 2160", "FHD 1080p", "HD 720p", "HQ 480p", "LQ 360p"}
+ *     enum={"4K 2160", "FHD 1080p", "HD 720p", "HQ 480p", "LQ 360p"},
+ *     example="4K 2160p",
  *   ),
- *   @OA\Property(property="title", type="string"),
- *   @OA\Property(property="dateInitFinishedRaw", type="string", format="date-time"),
- *   @OA\Property(property="dateInitFinished", type="string", format="date-time"),
- *   @OA\Property(property="dateLastFinished", type="string", format="date-time"),
- *   @OA\Property(property="durationRaw", type="integer", format="int64"),
- *   @OA\Property(property="duration", type="string"),
- *   @OA\Property(property="filesizeRaw", type="integer", format="int64"),
- *   @OA\Property(property="filesize", type="string"),
+ *   @OA\Property(property="title", type="string", example="Sample Title"),
+ *   @OA\Property(
+ *     property="dateInitFinishedRaw",
+ *     type="string",
+ *     format="date-time",
+ *     example="2001-01-01T00:00:00.000000Z",
+ *   ),
+ *   @OA\Property(property="dateInitFinished", type="string", example="Jan 01, 2001"),
+ *   @OA\Property(property="dateLastFinished", type="string", example="Mar 01, 2011"),
+ *   @OA\Property(
+ *     property="durationRaw",
+ *     type="integer",
+ *     format="int64",
+ *     example=10000,
+ *   ),
+ *   @OA\Property(
+ *     property="duration",
+ *     type="string",
+ *     example="2 hours 46 minutes 40 seconds",
+ *   ),
+ *   @OA\Property(
+ *     property="filesizeRaw",
+ *     type="integer",
+ *     format="int64",
+ *     example=21331439861,
+ *   ),
+ *   @OA\Property(property="filesize", type="string", example="19.87 GB"),
  *
- *   @OA\Property(property="episodes", type="integer", format="int32"),
- *   @OA\Property(property="ovas", type="integer", format="int32"),
- *   @OA\Property(property="specials", type="integer", format="int32"),
+ *   @OA\Property(property="episodes", type="integer", format="int32", example=25),
+ *   @OA\Property(property="ovas", type="integer", format="int32", example=1),
+ *   @OA\Property(property="specials", type="integer", format="int32", example=1),
  *
- *   @OA\Property(property="seasonNumber", type="integer", format="int32"),
- *   @OA\Property(property="seasonFirstTitle", type="string"),
+ *   @OA\Property(property="seasonNumber", type="integer", format="int32", example=1),
+ *   @OA\Property(property="seasonFirstTitle", type="string", example="Sample Title"),
  *
- *   @OA\Property(property="prequelTitle", type="string"),
+ *   @OA\Property(property="prequelTitle", type="string", example="Prequel Title"),
  *   @OA\Property(
  *     property="prequel",
- *     @OA\Property(property="id", type="string", format="uuid"),
- *     @OA\Property(property="title", type="string"),
+ *     @OA\Property(
+ *       property="id",
+ *       type="string",
+ *       format="uuid",
+ *       example="9b712dbe-ff81-41d4-b027-ee87cea4da99",
+ *     ),
+ *     @OA\Property(property="title", type="string", example="Prequel Title"),
  *   ),
- *
- *   @OA\Property(property="sequelTitle", type="string"),
+ *   @OA\Property(property="sequelTitle", type="string", example="Sequel Title"),
  *   @OA\Property(
  *     property="sequel",
- *     @OA\Property(property="id", type="string", format="uuid"),
- *     @OA\Property(property="title", type="string"),
+ *     @OA\Property(
+ *       property="id",
+ *       type="string",
+ *       format="uuid",
+ *       example="08d0f87f-6432-47bc-b6bd-c856abbad90f",
+ *     ),
+ *     @OA\Property(property="title", type="string", example="Sequel Title"),
  *   ),
  *
- *   @OA\Property(property="encoder", type="string"),
- *   @OA\Property(property="encoderVideo", type="string"),
- *   @OA\Property(property="encoderAudio", type="string"),
- *   @OA\Property(property="encoderSubs", type="string"),
+ *   @OA\Property(property="encoder", type="string", example="encoder-encoder2-encoder3"),
+ *   @OA\Property(property="encoderVideo", type="string", example="encoder"),
+ *   @OA\Property(property="encoderAudio", type="string", example="encoder2"),
+ *   @OA\Property(property="encoderSubs", type="string", example="encoder3"),
  *
  *   @OA\Property(
  *     property="releaseSeason",
  *     type="string",
- *     enum={"Winter", "Spring", "Summer", "Fall"}
+ *     enum={"Winter", "Spring", "Summer", "Fall"},
+ *     example="Winter",
  *   ),
- *   @OA\Property(property="releaseYear", type="integer", format="int32"),
- *   @OA\Property(property="release", type="string"),
+ *   @OA\Property(property="releaseYear", type="integer", format="int32", example=2000),
+ *   @OA\Property(property="release", type="string", example="Winter 2000"),
  *
- *   @OA\Property(property="variants", type="string"),
- *   @OA\Property(property="remarks", type="string"),
+ *   @OA\Property(property="variants", type="string", example="Variant title"),
+ *   @OA\Property(property="remarks", type="string", example="Some remarks"),
  *
- *   @OA\Property(property="codecHDR", type="integer", format="int32"),
- *   @OA\Property(property="id_codec_video", type="integer", format="int32"),
- *   @OA\Property(property="codecVideo", type="string"),
- *   @OA\Property(property="id_codec_audio", type="integer", format="int32"),
- *   @OA\Property(property="codecAudio", type="string"),
+ *   @OA\Property(property="codecHDR", type="integer", format="int32", example=1),
+ *   @OA\Property(property="id_codec_video", type="integer", format="int32", example=1),
+ *   @OA\Property(property="codecVideo", type="string", example="x264 8bit"),
+ *   @OA\Property(property="id_codec_audio", type="integer", format="int32", example=4),
+ *   @OA\Property(property="codecAudio", type="string", example="FLAC 7.1"),
  *
- *   @OA\Property(property="offquels", ref="#/components/schemas/EntryOffquelCollection"),
- *   @OA\Property(property="rewatches", ref="#/components/schemas/EntryRewatchCollection"),
+ *   @OA\Property(
+ *     property="offquels",
+ *     type="array",
+ *     @OA\Items(ref="#/components/schemas/EntryOffquelResource"),
+ *   ),
+ *   @OA\Property(
+ *     property="rewatches",
+ *     type="array",
+ *     @OA\Items(ref="#/components/schemas/EntryRewatchResource")
+ *   ),
  *
- *   @OA\Property(property="ratingAverage", type="number"),
+ *   @OA\Property(property="ratingAverage", type="number", example=4.5),
  *   @OA\Property(
  *     property="rating",
- *     @OA\Property(property="audio", type="integer", format="int32"),
- *     @OA\Property(property="enjoyment", type="integer", format="int32"),
- *     @OA\Property(property="graphics", type="integer", format="int32"),
- *     @OA\Property(property="plot", type="integer", format="int32"),
+ *     @OA\Property(property="audio", type="integer", format="int32", example=6),
+ *     @OA\Property(property="enjoyment", type="integer", format="int32", example=5),
+ *     @OA\Property(property="graphics", type="integer", format="int32", example=4),
+ *     @OA\Property(property="plot", type="integer", format="int32", example=3),
  *   ),
- *   @OA\Property(property="image", type="string", format="uri"),
+ *   @OA\Property(property="image", type="string", format="uri", example="{{ image url }}"),
  * ),
  */
 class EntryResource extends JsonResource {
@@ -202,8 +178,8 @@ class EntryResource extends JsonResource {
       'id_codec_audio' => $this->id_codec_audio,
       'codecAudio' => $this->codec_audio->codec ?? '',
 
-      'offquels' => EntryOffquelCollection::collection($this->offquels),
-      'rewatches' => EntryRewatchCollection::collection($this->rewatches),
+      'offquels' => EntryOffquelResource::collection($this->offquels),
+      'rewatches' => EntryRewatchResource::collection($this->rewatches),
 
       'ratingAverage' => $this->calcRating(),
       'rating' => $this->rating,
