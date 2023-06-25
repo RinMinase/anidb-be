3c798a48645ff732e7dae1aa920ae69640313fc3
Rin Minase <rin.black.raison.detre@gmail.com>
2023-06-25 14:32:49 +0800

    ✨ feat: improved year validation rule

--- app/Rules/YearRule.php
+++ app/Rules/YearRule.php
@@ -8,7 +8,11 @@ use Illuminate\Contracts\Validation\ValidationRule;
 class YearRule implements ValidationRule {
 
   public function validate(string $attribute, mixed $value, Closure $fail): void {
-    if (!is_int($value) || $value < 1900 || $value > 2999) {
+    if (!is_numeric($value)) {
+      $fail($attribute . ' should be an integer');
+    }
+
+    if (intval($value) < 1900 || intval($value) > 2999) {
       $fail($attribute . ' should be a valid year format from 1900-2999');
     }
   }
