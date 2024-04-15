5923a1a1d82c5b8f5cb4fe5f0450ff70d06002c2
Rin Minase <rin.black.raison.detre@gmail.com>
2024-04-15 14:55:47 +0800

    ✨ feat(fourleaf_gas): added model

---
+++ app/Fourleaf/Models/MaintenancePart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Fourleaf\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @OA\Schema(
+ *   @OA\Property(property="date", type="string", example="2020-10-20"),
+ *   @OA\Property(property="part", type="string", example="Engine Oil Change"),
+ *   @OA\Property(property="odometer", type="integer", format="int32", minimum=0, example=1000),
+ * )
+ */
+class MaintenancePart extends Model {
+
+  protected $table = 'fourleaf_maintenance_parts';
+  public $timestamps = null;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'part',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [];
+}
