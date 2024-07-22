047b29a57b0ea5535689c9f7bf8f7ac47f4ca4ff
Rin Minase <rin.black.raison.detre@gmail.com>
2024-07-23 03:23:33 +0800

    🐛 fix: fixed validation rules

--- app/Rules/DivisibleBy15Rule.php
+++ app/Rules/DivisibleBy15Rule.php
@@ -5,10 +5,12 @@ namespace App\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
+use function Fuse\Helpers\Types\isNumber;
+
 class DivisibleBy15Rule implements ValidationRule {
 
   public function validate(string $attribute, mixed $value, Closure $fail): void {
-    if ($value % 15 !== 0) {
+    if (!isNumber($value) || $value % 15 !== 0) {
       $fail($attribute . ' must be divisible by 15');
     }
   }
