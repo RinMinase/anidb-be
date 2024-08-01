143fe3b48ab4614c8fc700020aa0607bcf6a224e
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-02 02:34:15 +0800

    ✨ feat: added cockroachdb config

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
       'sslmode' => 'prefer',
     ],
 
+    'crdb' => [
+      'driver' => 'crdb',
+      'url' => env('DATABASE_URL'),
+      'host' => env('DB_HOST', '127.0.0.1'),
+      'port' => env('DB_PORT', '26257'),
+      'database' => env('DB_DATABASE', 'forge'),
+      'username' => env('DB_USERNAME', 'forge'),
+      'password' => env('DB_PASSWORD', ''),
+      'charset' => 'utf8',
+      'prefix' => '',
+      'prefix_indexes' => true,
+      'schema' => 'public',
+      'sslmode' => 'prefer',
+    ]
+
   ],
 
   /*
