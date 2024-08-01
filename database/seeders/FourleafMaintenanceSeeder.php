acc11322c7e1f5be8580285a27773f603647bd50
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-02 04:09:35 +0800

    🐛 fix(fourleaf_gas): fixed seeder fragility

--- database/seeders/FourleafMaintenanceSeeder.php
+++ database/seeders/FourleafMaintenanceSeeder.php
@@ -30,12 +30,24 @@ class FourleafMaintenanceSeeder extends Seeder {
       Maintenance::create($item);
     }
 
+    $id_fourleaf_maintenance_1 = Maintenance::where('date', $testData[0]['date'])
+      ->where('description', $testData[0]['description'])
+      ->where('odometer', $testData[0]['odometer'])
+      ->first()
+      ->id;
+
+    $id_fourleaf_maintenance_2 = Maintenance::where('date', $testData[1]['date'])
+      ->where('description', $testData[1]['description'])
+      ->where('odometer', $testData[1]['odometer'])
+      ->first()
+      ->id;
+
     $testDataParts = [
       [
-        'id_fourleaf_maintenance' => 1,
+        'id_fourleaf_maintenance' => $id_fourleaf_maintenance_1,
         'part' => 'engine_oil',
       ], [
-        'id_fourleaf_maintenance' => 2,
+        'id_fourleaf_maintenance' => $id_fourleaf_maintenance_2,
         'part' => 'engine_oil',
       ],
     ];
