398d874a7e38e5702fc098227ec64734806ab7aa
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-03 08:06:14 +0800

    🧪 test: added test case for alpha only flag

--- tests/Unit/RandomStringTest.php
+++ tests/Unit/RandomStringTest.php
@@ -28,6 +28,28 @@ class RandomStringTest extends TestCase {
     $this->assertEquals($test_length, strlen($actual));
   }
 
+  public function test_should_create_a_random_string_of_only_characters_when_alpha_only_is_true() {
+    $test_length = 1;
+
+    for ($i = 0; $i < 30; $i++) {
+      $actual = rand_str($test_length, true);
+
+      $this->assertEquals($test_length, strlen($actual));
+      $this->assertTrue(ctype_lower($actual));
+    }
+  }
+
+  public function test_should_create_a_random_string_of_alphanumeric_characters_when_alpha_only_is_false() {
+    $test_length = 1;
+
+    for ($i = 0; $i < 30; $i++) {
+      $actual = rand_str($test_length, false);
+
+      $this->assertEquals($test_length, strlen($actual));
+      $this->assertTrue(ctype_alnum($actual));
+    }
+  }
+
   public function test_should_create_a_random_string_of_length_20_when_no_args_provided() {
     $expected_length = 20;
     $actual = rand_str();
