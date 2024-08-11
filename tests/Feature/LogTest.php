8b94d2772d64e5eb8c509b5e9c44541c3d551977
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-12 04:27:38 +0800

    🧪 test(log): improved test cases

--- tests/Feature/LogTest.php
+++ tests/Feature/LogTest.php
@@ -9,19 +9,42 @@ use App\Models\Log;
 
 class LogTest extends BaseTestCase {
 
+  // Backup related variables
+  private $log_backup = null;
+
+  // Class variables
   private $log_id_1 = 99999;
   private $log_id_2 = 99998;
   private $log_id_3 = 99997;
+
+  private $log_uuid_1 = '1957ee99-e510-4049-b2f3-1ac113133228';
+  private $log_uuid_2 = '6e6b1f31-6ba7-4ee4-88b6-d837ae424ac3';
+  private $log_uuid_3 = 'e97f0a9d-9be0-417f-a919-b152262068aa';
+
   private $log_created_at_1 = '2090-01-01 13:00:00';
   private $log_created_at_2 = '2089-12-31 13:00:00';
   private $log_created_at_3 = '2089-12-30 13:00:00';
 
+  // Backup related tables
+  private function setup_backup() {
+    $hidden_columns = ['id'];
+    $this->log_backup = Log::all()->makeVisible($hidden_columns)->toArray();
+  }
+
+  // Restore related tables
+  private function setup_restore() {
+    Log::truncate();
+    Log::insert($this->log_backup);
+    Log::refreshAutoIncrements();
+  }
+
+  // Setup data for testing
   private function setup_config() {
-    // Clearing possible duplicate data
-    $this->setup_clear();
+    Log::truncate();
 
     Log::insert([[
       'id' => $this->log_id_1,
+      'uuid' => $this->log_uuid_1,
       'table_changed' => 'hdd',
       'id_changed' => '8eab429d-edc7-4c11-a94c-30903dd65dc6',
       'description' => 'description',
@@ -29,6 +52,7 @@ class LogTest extends BaseTestCase {
       'created_at' => $this->log_created_at_1,
     ], [
       'id' => $this->log_id_2,
+      'uuid' => $this->log_uuid_2,
       'table_changed' => 'entry',
       'id_changed' => 'e17d35dd-e9cc-4098-97e5-4891083038a1',
       'description' => 'description',
@@ -36,6 +60,7 @@ class LogTest extends BaseTestCase {
       'created_at' => $this->log_created_at_2,
     ], [
       'id' => $this->log_id_3,
+      'uuid' => $this->log_uuid_3,
       'table_changed' => 'entry',
       'id_changed' => '22728a9f-7ee7-4290-8b14-1cbe7c780a6e',
       'description' => 'description',
@@ -44,11 +69,18 @@ class LogTest extends BaseTestCase {
     ]]);
   }
 
-  private function setup_clear() {
-    Log::whereIn('id', [$this->log_id_1, $this->log_id_2, $this->log_id_3])
-      ->forceDelete();
+  // Fixtures
+  public function setUp(): void {
+    parent::setUp();
+    $this->setup_backup();
   }
 
+  public function tearDown(): void {
+    $this->setup_restore();
+    parent::tearDown();
+  }
+
+  // Test Cases
   public function test_should_get_all_data() {
     $this->setup_config();
 
@@ -74,7 +106,15 @@ class LogTest extends BaseTestCase {
         ]
       ]);
 
-    $this->setup_clear();
+    $actual_log_ids = collect($response['data'])->pluck('id')->toArray();
+
+    $expected_log_ids = [
+      $this->log_uuid_1,
+      $this->log_uuid_2,
+      $this->log_uuid_3,
+    ];
+
+    $this->assertEqualsCanonicalizing($expected_log_ids, $actual_log_ids);
   }
 
   public function test_should_get_all_data_sorted_by_column() {
@@ -104,12 +144,10 @@ class LogTest extends BaseTestCase {
         ]
       ]);
 
-    $this->assertSame(
+    $this->assertEquals(
       Carbon::parse($this->log_created_at_1)->toString(),
       Carbon::parse($response['data'][0]['createdAt'])->toString(),
     );
-
-    $this->setup_clear();
   }
 
   public function test_should_get_all_data_ordered() {
@@ -143,12 +181,10 @@ class LogTest extends BaseTestCase {
         ]
       ]);
 
-    $this->assertSame(
+    $this->assertEquals(
       Carbon::parse($this->log_created_at_1)->toString(),
       Carbon::parse($response['data'][0]['createdAt'])->toString(),
     );
-
-    $this->setup_clear();
   }
 
   public function test_should_get_all_data_by_page() {
@@ -186,16 +222,13 @@ class LogTest extends BaseTestCase {
         ]
       ]);
 
-    $this->assertSame(
+    $this->assertEquals(
       Carbon::parse($this->log_created_at_2)->toString(),
       Carbon::parse($response['data'][0]['createdAt'])->toString(),
     );
-
-    $this->setup_clear();
   }
 
   public function test_should_get_all_data_with_limit() {
-
     $this->setup_config();
 
     $test_column = 'created_at';
@@ -230,11 +263,6 @@ class LogTest extends BaseTestCase {
         ]
       ]);
 
-    $this->assertCount(
-      $test_limit,
-      $response['data'],
-    );
-
-    $this->setup_clear();
+    $this->assertCount($test_limit, $response['data'],);
   }
 }
