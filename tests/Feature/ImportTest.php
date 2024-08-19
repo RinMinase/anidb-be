8b1c7fa72ea8f3acd48f7b05c6d57f815a550cfe
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-19 21:09:23 +0800

    🧪 test(import): appended blank test case template

---
+++ tests/Feature/ImportTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\BaseTestCase;
+
+class ImportTest extends BaseTestCase {
+
+  // Buckets
+  public function test_should_import_buckets() {
+  }
+
+  public function test_should_not_import_buckets_when_no_file_is_attached() {
+  }
+
+  public function test_should_not_import_buckets_when_file_type_is_invalid() {
+  }
+
+  public function test_should_not_import_buckets_when_json_content_is_invalid() {
+  }
+
+  // Groups
+  public function test_should_import_groups() {
+  }
+
+  public function test_should_not_import_groups_when_no_file_is_attached() {
+  }
+
+  public function test_should_not_import_groups_when_file_type_is_invalid() {
+  }
+
+  public function test_should_not_import_groups_when_json_content_is_invalid() {
+  }
+
+  // PC Setups
+  public function test_should_import_pc_setups() {
+  }
+
+  public function test_should_not_import_pc_setups_when_no_file_is_attached() {
+  }
+
+  public function test_should_not_import_pc_setups_when_file_type_is_invalid() {
+  }
+
+  public function test_should_not_import_pc_setups_when_json_content_is_invalid() {
+  }
+
+  // Sequences
+  public function test_should_import_sequences() {
+  }
+
+  public function test_should_not_import_sequences_when_no_file_is_attached() {
+  }
+
+  public function test_should_not_import_sequences_when_file_type_is_invalid() {
+  }
+
+  public function test_should_not_import_sequences_when_json_content_is_invalid() {
+  }
+
+  // Entries
+  public function test_should_import_entries() {
+  }
+
+  public function test_should_not_import_entries_when_no_file_is_attached() {
+  }
+
+  public function test_should_not_import_entries_when_file_type_is_invalid() {
+  }
+
+  public function test_should_not_import_entries_when_json_content_is_invalid() {
+  }
+}
