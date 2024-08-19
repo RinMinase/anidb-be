f1c9d9337ce5fdedbaee5e4aeba69740047bb6aa
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-19 20:04:49 +0800

    🧪 test(management): improved test cases

--- tests/Feature/ManagementTest.php
+++ tests/Feature/ManagementTest.php
@@ -2,6 +2,14 @@
 
 namespace Tests\Feature;
 
+
+use Tests\BaseTestCase;
+
+use Database\Seeders\BucketSeeder;
+use Database\Seeders\CatalogSeeder;
+use Database\Seeders\EntrySeeder;
+use Database\Seeders\PartialSeeder;
+
 use App\Models\Bucket;
 use App\Models\Catalog;
 use App\Models\Entry;
@@ -9,14 +17,101 @@ use App\Models\EntryOffquel;
 use App\Models\EntryRating;
 use App\Models\EntryRewatch;
 use App\Models\Partial;
-use Database\Seeders\BucketSeeder;
-use Database\Seeders\CatalogSeeder;
-use Database\Seeders\EntrySeeder;
-use Database\Seeders\PartialSeeder;
-use Tests\BaseTestCase;
 
 class ManagementTest extends BaseTestCase {
+
+  // Backup related variables
+  private $entry_rewatch_backup = null;
+  private $entry_rating_backup = null;
+  private $entry_offquel_backup = null;
+  private $entry_backup = null;
+
+  private $buckets_backup = null;
+  private $partials_backup = null;
+  private $catalogs_backup = null;
+
+  // Backup related tables
+  private function setup_backup() {
+    $hidden_columns = ['id', 'id_entries'];
+    $this->entry_rewatch_backup = EntryRewatch::all()->makeVisible($hidden_columns)->toArray();
+
+    $hidden_columns = ['id', 'id_entries', 'created_at', 'updated_at', 'deleted_at'];
+    $this->entry_rating_backup = EntryRating::all()->makeVisible($hidden_columns)->toArray();
+
+    $hidden_columns = ['id_entries', 'created_at', 'updated_at', 'deleted_at'];
+    $this->entry_offquel_backup = EntryOffquel::all()->makeVisible($hidden_columns)->toArray();
+
+    $hidden_columns = ['id', 'id_quality', 'updated_at', 'deleted_at'];
+    $this->entry_backup = Entry::all()->makeVisible($hidden_columns)->toArray();
+
+    $this->buckets_backup = Bucket::all()->toArray();
+
+    $hidden_columns = ['id', 'created_at', 'updated_at', 'deleted_at'];
+    $this->partials_backup = Partial::all()->makeVisible($hidden_columns)->toArray();
+
+    $hidden_columns = ['id', 'updated_at', 'deleted_at'];
+    $this->catalogs_backup = Catalog::all()->makeVisible($hidden_columns)->toArray();
+  }
+
+  private function setup_restore() {
+    Entry::truncate();
+    Bucket::truncate();
+    Partial::truncate();
+    Catalog::truncate();
+
+    Entry::insert($this->entry_backup);
+    EntryOffquel::insert($this->entry_offquel_backup);
+    EntryRating::insert($this->entry_rating_backup);
+    EntryRewatch::insert($this->entry_rewatch_backup);
+    Bucket::insert($this->buckets_backup);
+    Catalog::insert($this->catalogs_backup);
+    Partial::insert($this->partials_backup);
+
+    Entry::refreshAutoIncrements();
+    EntryOffquel::refreshAutoIncrements();
+    EntryRating::refreshAutoIncrements();
+    EntryRewatch::refreshAutoIncrements();
+    Bucket::refreshAutoIncrements();
+    Catalog::refreshAutoIncrements();
+    Partial::refreshAutoIncrements();
+  }
+
+  // Setup data for testing
+  private function setup_config() {
+    Entry::truncate();
+    Bucket::truncate();
+    Partial::truncate();
+    Catalog::truncate();
+
+    Entry::refreshAutoIncrements();
+    EntryOffquel::refreshAutoIncrements();
+    EntryRating::refreshAutoIncrements();
+    EntryRewatch::refreshAutoIncrements();
+    Bucket::refreshAutoIncrements();
+    Catalog::refreshAutoIncrements();
+    Partial::refreshAutoIncrements();
+
+    (new EntrySeeder())->run();
+    (new BucketSeeder())->run();
+    (new CatalogSeeder())->run();
+    (new PartialSeeder())->run();
+  }
+
+  // Fixtures
+  public function setUp(): void {
+    parent::setUp();
+    $this->setup_backup();
+  }
+
+  public function tearDown(): void {
+    $this->setup_restore();
+    parent::tearDown();
+  }
+
+  // Test Cases
   public function test_should_get_all_managment_data() {
+    $this->setup_config();
+
     $response = $this->withoutMiddleware()->get('/api/management');
 
     $response->assertStatus(200)
@@ -68,142 +163,69 @@ class ManagementTest extends BaseTestCase {
   }
 
   public function test_should_validate_management_stats() {
-    // Backup related variables
-    $entry_rewatch_backup = null;
-    $entry_rating_backup = null;
-    $entry_offquel_backup = null;
-    $entry_backup = null;
+    $this->setup_config();
 
-    $buckets_backup = null;
-    $partials_backup = null;
-    $catalogs_backup = null;
+    $response = $this->withoutMiddleware()->get('/api/management');
 
-    // Save current data and relations
-    $entry_rewatch_backup = EntryRewatch::all()
-      ->makeVisible(['id', 'id_entries'])
-      ->toArray();
+    $expected_count = [
+      'entries' => 9,
+      'buckets' => 6,
+      'partials' => 4,
+    ];
 
-    $entry_rating_backup = EntryRating::all()
-      ->makeVisible(['id', 'id_entries', 'created_at', 'updated_at', 'deleted_at'])
-      ->toArray();
+    $expected_stats = [
+      'watchSeconds' => 1080000,
+      'watch' => '12 days',
+      'watchSubtext' => '12 hours',
+      'rewatchSeconds' => 2100000,
+      'rewatch' => '24 days',
+      'rewatchSubtext' => '7 hours 20 minutes',
+      'bucketSize' => '10.92 TB',
+      'entrySize' => '365.23 GB',
+      'episodes' => 90,
+      'titles' => 9,
+      'seasons' => 7,
+    ];
 
-    $entry_offquel_backup = EntryOffquel::all()
-      ->makeVisible(['id_entries', 'created_at', 'updated_at', 'deleted_at'])
-      ->toArray();
+    $expected_graph_quality = [
+      'quality2160' => 1,
+      'quality1080' => 2,
+      'quality720' => 2,
+      'quality480' => 3,
+      'quality360' => 1,
+    ];
 
-    $entry_backup = Entry::all()
-      ->makeVisible(['id', 'id_quality', 'updated_at', 'deleted_at'])
-      ->toArray();
+    $expected_graph_months = [
+      'jan' => 4,
+      'feb' => 0,
+      'mar' => 0,
+      'apr' => 1,
+      'may' => 1,
+      'jun' => 1,
+      'jul' => 1,
+      'aug' => 1,
+      'sep' => 0,
+      'oct' => 0,
+      'nov' => 0,
+      'dec' => 0,
+    ];
 
-    $buckets_backup = Bucket::all()->toArray();
+    $response->assertStatus(200);
 
-    $partials_backup = Partial::all()
-      ->makeVisible(['id', 'created_at', 'updated_at', 'deleted_at'])
-      ->toArray();
+    $actual = $response['data'];
 
-    $catalogs_backup = Catalog::all()
-      ->makeVisible(['id', 'updated_at', 'deleted_at'])
-      ->toArray();
+    $this->assertNotNull($actual['count']);
+    $this->assertEquals($expected_count, $actual['count']);
 
-    try {
-      // Remove existing values
-      Entry::truncate();
-      Bucket::truncate();
-      Partial::truncate();
-      Catalog::truncate();
+    $this->assertNotNull($actual['stats']);
+    $this->assertEquals($expected_stats, $actual['stats']);
 
-      // Add testing values -- using the seeders
-      (new EntrySeeder())->run();
-      (new BucketSeeder())->run();
-      (new CatalogSeeder())->run();
-      (new PartialSeeder())->run();
+    $this->assertNotNull($actual['graph']);
+    $this->assertNotNull($actual['graph']['quality']);
+    $this->assertEquals($expected_graph_quality, $actual['graph']['quality']);
 
-      // Test the calculations with the seeded data
-      $response = $this->withoutMiddleware()->get('/api/management');
-
-      $expected_count = [
-        'entries' => 9,
-        'buckets' => 6,
-        'partials' => 4,
-      ];
-
-      $expected_stats = [
-        'watchSeconds' => 1080000,
-        'watch' => '12 days',
-        'watchSubtext' => '12 hours',
-        'rewatchSeconds' => 2100000,
-        'rewatch' => '24 days',
-        'rewatchSubtext' => '7 hours 20 minutes',
-        'bucketSize' => '10.92 TB',
-        'entrySize' => '365.23 GB',
-        'episodes' => 90,
-        'titles' => 9,
-        'seasons' => 7,
-      ];
-
-      $expected_graph_quality = [
-        'quality2160' => 1,
-        'quality1080' => 2,
-        'quality720' => 2,
-        'quality480' => 3,
-        'quality360' => 1,
-      ];
-
-      $expected_graph_months = [
-        'jan' => 4,
-        'feb' => 0,
-        'mar' => 0,
-        'apr' => 1,
-        'may' => 1,
-        'jun' => 1,
-        'jul' => 1,
-        'aug' => 1,
-        'sep' => 0,
-        'oct' => 0,
-        'nov' => 0,
-        'dec' => 0,
-      ];
-
-      $response->assertStatus(200);
-
-      $actual = $response['data'];
-
-      $this->assertNotNull($actual['count']);
-      $this->assertEquals($expected_count, $actual['count']);
-
-      $this->assertNotNull($actual['stats']);
-      $this->assertEquals($expected_stats, $actual['stats']);
-
-      $this->assertNotNull($actual['graph']);
-      $this->assertNotNull($actual['graph']['quality']);
-      $this->assertEquals($expected_graph_quality, $actual['graph']['quality']);
-
-      $this->assertNotNull($actual['graph']);
-      $this->assertNotNull($actual['graph']['months']);
-      $this->assertEquals($expected_graph_months, $actual['graph']['months']);
-    } finally {
-      // Remove test data values
-      Entry::truncate();
-      Bucket::truncate();
-      Partial::truncate();
-      Catalog::truncate();
-
-      // Restore initially saved data
-      Entry::insert($entry_backup);
-      EntryOffquel::insert($entry_offquel_backup);
-      EntryRating::insert($entry_rating_backup);
-      EntryRewatch::insert($entry_rewatch_backup);
-      Bucket::insert($buckets_backup);
-      Catalog::insert($catalogs_backup);
-      Partial::insert($partials_backup);
-
-      refresh_db_table_autoincrement((new Entry())->getTable());
-      refresh_db_table_autoincrement((new EntryOffquel())->getTable());
-      refresh_db_table_autoincrement((new EntryRating())->getTable());
-      refresh_db_table_autoincrement((new EntryRewatch())->getTable());
-      refresh_db_table_autoincrement((new Bucket())->getTable());
-      refresh_db_table_autoincrement((new Catalog())->getTable());
-      refresh_db_table_autoincrement((new Partial())->getTable());
-    }
+    $this->assertNotNull($actual['graph']);
+    $this->assertNotNull($actual['graph']['months']);
+    $this->assertEquals($expected_graph_months, $actual['graph']['months']);
   }
 }
