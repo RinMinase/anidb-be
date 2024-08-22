38deba1ef80a0f6db6fe9fafb9b05fc905dd741c
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-22 19:35:57 +0800

    📝 docs(sequence): updated api docs

--- app/Models/Sequence.php
+++ app/Models/Sequence.php
@@ -10,8 +10,8 @@ use App\Traits\RefreshableAutoIncrements;
  * @OA\Schema(
  *   @OA\Property(property="id", type="integer", format="int32", example=1),
  *   @OA\Property(property="title", type="string", example="Summer List"),
- *   @OA\Property(property="date_from", type="string", format="date", example="2020-01-01"),
- *   @OA\Property(property="date_to", type="string", format="date", example="2020-02-01"),
+ *   @OA\Property(property="dateFrom", type="string", format="date", example="2020-01-01"),
+ *   @OA\Property(property="dateTo", type="string", format="date", example="2020-02-01"),
  * )
  */
 class Sequence extends Model {
