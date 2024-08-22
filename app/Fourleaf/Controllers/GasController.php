4945301c28cd131312b12d18d3d0e499be845e4f
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-23 01:22:11 +0800

    📝 docs(fourleaf_gas): updated api docs

--- app/Fourleaf/Controllers/GasController.php
+++ app/Fourleaf/Controllers/GasController.php
@@ -14,7 +14,6 @@ use App\Fourleaf\Requests\Gas\AddEditFuelRequest;
 use App\Fourleaf\Requests\Gas\AddEditMaintenanceRequest;
 use App\Fourleaf\Requests\Gas\GetRequest;
 
-use App\Fourleaf\Resources\Gas\GetResource;
 use App\Fourleaf\Resources\Gas\MaintenanceResource;
 
 class GasController extends Controller {
@@ -23,6 +22,7 @@ class GasController extends Controller {
   public function __construct(GasRepository $gasRepository) {
     $this->gasRepository = $gasRepository;
   }
+
 
   /**
    * @OA\Get(
@@ -42,7 +42,122 @@ class GasController extends Controller {
    *         @OA\Schema(
    *           @OA\Property(
    *             property="data",
-   *             ref="#/components/schemas/FourleafGasGetResource",
+   *
+   *             @OA\Property(
+   *               property="stats",
+   *               @OA\Property(property="averageEfficiency", type="float", minimum=0, example=12.23),
+   *               @OA\Property(property="lastEfficiency", type="float", minimum=0, example=12.23),
+   *               @OA\Property(property="mileage", type="integer", format="int32", minimum=0, example=1000),
+   *               @OA\Property(property="age", type="string", example="1 year, 2 months"),
+   *               @OA\Property(property="kmPerMonth", type="float", minimum=0, example=12.23),
+   *             ),
+   *
+   *             @OA\Property(
+   *               property="graph",
+   *               @OA\Property(
+   *                 property="efficiency",
+   *                 @OA\Property(property="<date>", type="string", example="<value>"),
+   *                 @OA\Property(property="2020-10-20", type="float", example=12.23),
+   *               ),
+   *               @OA\Property(
+   *                 property="gas",
+   *                 @OA\Property(property="<date>", type="string", example="<value>"),
+   *                 @OA\Property(property="2020-10-20", type="float", example=12.23),
+   *               ),
+   *               @OA\Property(
+   *                 property="odometer",
+   *                 type="array",
+   *                 example={123, 234},
+   *                 @OA\Items(schema="integer")
+   *               ),
+   *             ),
+   *
+   *             @OA\Property(
+   *               property="maintenance",
+   *               @OA\Property(
+   *                 property="km",
+   *                 @OA\Property(property="engineOil", type="string", example="normal"),
+   *                 @OA\Property(property="tires", type="string", example="normal"),
+   *                 @OA\Property(property="transmissionFluid", type="string", example="normal"),
+   *                 @OA\Property(property="brakeFluid", type="string", example="normal"),
+   *                 @OA\Property(property="radiatorFluid", type="string", example="normal"),
+   *                 @OA\Property(property="sparkPlugs", type="string", example="normal"),
+   *                 @OA\Property(property="powerSteeringFluid", type="string", example="normal"),
+   *               ),
+   *               @OA\Property(
+   *                 property="year",
+   *                 @OA\Property(property="engineOil", type="string", example="normal"),
+   *                 @OA\Property(property="transmissionFluid", type="string", example="normal"),
+   *                 @OA\Property(property="brakeFluid", type="string", example="normal"),
+   *                 @OA\Property(property="battery", type="string", example="normal"),
+   *                 @OA\Property(property="radiatorFluid", type="string", example="normal"),
+   *                 @OA\Property(property="acCoolant", type="string", example="normal"),
+   *                 @OA\Property(property="powerSteeringFluid", type="string", example="normal"),
+   *                 @OA\Property(property="tires", type="string", example="normal"),
+   *               ),
+   *             ),
+   *
+   *             @OA\Property(
+   *               property="lastMaintenance",
+   *               @OA\Property(
+   *                 property="km",
+   *                 @OA\Property(property="engineOil", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="tires", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="transmissionFluid", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="brakeFluid", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="radiatorFluid", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="sparkPlugs", type="string", example="2024-01-01"),
+   *                 @OA\Property(property="powerSteeringFluid", type="string", example="2024-01-01"),
+   *               ),
+   *               @OA\Property(
+   *                 property="year",
+   *                 @OA\Property(
+   *                   property="acCoolant",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="battery",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="brakeFluid",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="engineOil",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="powerSteeringFluid",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="radiatorFluid",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="sparkPlugs",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="tires",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *                 @OA\Property(
+   *                   property="transmissionFluid",
+   *                   @OA\Property(property="date", type="string", example="2024-01-01"),
+   *                   @OA\Property(property="odometer", type="number", minimum=0, maximum=100000, example=2000),
+   *                 ),
+   *               ),
+   *             ),
    *           ),
    *         ),
    *       },
--- app/Fourleaf/Models/Gas.php
+++ app/Fourleaf/Models/Gas.php
@@ -8,12 +8,13 @@ use App\Traits\RefreshableAutoIncrements;
 
 /**
  * @OA\Schema(
+ *   @OA\Property(property="id", type="integer", format="int32", example=1),
  *   @OA\Property(property="date", type="string", example="2020-10-20"),
- *   @OA\Property(property="from_bars", type="integer", format="int32", minimum=0, maximum=9, example=1),
- *   @OA\Property(property="to_bars", type="integer", format="int32", minimum=0, maximum=9, example=1),
+ *   @OA\Property(property="fromBars", type="integer", format="int32", minimum=0, maximum=9, example=1),
+ *   @OA\Property(property="toBars", type="integer", format="int32", minimum=0, maximum=9, example=1),
  *   @OA\Property(property="odometer", type="integer", format="int32", minimum=0, example=1000),
- *   @OA\Property(property="price_per_liter", type="float", minimum=0, example=12.23),
- *   @OA\Property(property="liters_filled", type="float", minimum=0, example=12.23),
+ *   @OA\Property(property="pricePerLiter", type="float", minimum=0, example=12.23),
+ *   @OA\Property(property="litersFilled", type="float", minimum=0, example=12.23),
  * )
  */
 class Gas extends Model {
