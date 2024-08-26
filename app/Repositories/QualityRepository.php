68419899f3a90f5f02d2d633a92900f59fe0da84
Rin Minase <rin.black.raison.detre@gmail.com>
2024-08-26 18:28:24 +0800

    ✨ feat(quality): appended parsing function

--- app/Repositories/QualityRepository.php
+++ app/Repositories/QualityRepository.php
@@ -11,4 +11,36 @@ class QualityRepository {
       ->orderBy('id', 'asc')
       ->get();
   }
+
+  // Helper Functions
+  public static function parseQuality(string $quality): string {
+    $quality = strtolower($quality);
+
+    $values = ['4k', 'uhd', '2160p', '2160'];
+    if (in_array($quality, $values)) {
+      return Quality::where('vertical_pixels', 2160)->first()->quality;
+    }
+
+    $values = ['fhd', '1080p', '1080'];
+    if (in_array($quality, $values)) {
+      return Quality::where('vertical_pixels', 1080)->first()->quality;
+    }
+
+    $values = ['hd', '720p', '720'];
+    if (in_array($quality, $values)) {
+      return Quality::where('vertical_pixels', 720)->first()->quality;
+    }
+
+    $values = ['hq', '480p', '480'];
+    if (in_array($quality, $values)) {
+      return Quality::where('vertical_pixels', 480)->first()->quality;
+    }
+
+    $values = ['lq', '360p', '360'];
+    if (in_array($quality, $values)) {
+      return Quality::where('vertical_pixels', 360)->first()->quality;
+    }
+
+    return null;
+  }
 }
