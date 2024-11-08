1dc417c7999c73bb5c90fa0e7bf623be2b6f85f0
Rin Minase <rin.black.raison.detre@gmail.com>
2024-11-08 20:06:32 +0800

    ✨ feat: added helper function to check uuid validity

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -66,6 +66,16 @@ if (!function_exists('is_json')) {
   }
 }
 
+if (!function_exists('is_uuid')) {
+  function is_uuid(string $uuid): bool {
+    if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
 if (!function_exists('to_boolean')) {
   function to_boolean($value) {
     return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
