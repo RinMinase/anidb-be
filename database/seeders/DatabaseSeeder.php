085b6108e190a307db23d85ea694e32306f94c5a
Rin Minase <rin.black.raison.detre@gmail.com>
2024-11-19 21:53:47 +0800

    🐛 fix: reappended seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,16 +20,18 @@ class DatabaseSeeder extends Seeder {
 
       PCSetupInventoryTypeSeeder::class,
 
-      // BucketSeeder::class,      // test data
-      // BucketSimSeeder::class,   // test data
-      // CatalogSeeder::class,     // test data
-      // EntrySeeder::class,       // test data
-      // GroupSeeder::class,       // test data
-      // LogSeeder::class,         // test data
-      // PartialSeeder::class,     // test data
-      // SequenceSeeder::class,    // test data
+      BucketSeeder::class,      // test data
+      BucketSimSeeder::class,   // test data
+      CatalogSeeder::class,     // test data
+      EntrySeeder::class,       // test data
+      GroupSeeder::class,       // test data
+      LogSeeder::class,         // test data
+      PartialSeeder::class,     // test data
+      SequenceSeeder::class,    // test data
+
       RssSeeder::class,         // test data
       RssItemsSeeder::class,    // test data
+
       PCSetupSeeder::class,     // test data
       PCSetupInventorySeeder::class,     // test data
 
