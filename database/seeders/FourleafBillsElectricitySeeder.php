721d2c7b408ddde7e06c5e14877cb3280ab34a66
Rin Minase <rin.black.raison.detre@gmail.com>
2024-12-30 17:54:42 +0800

    ✨ feat(fourleaf_bills): updated seeder

--- database/seeders/FourleafBillsElectricitySeeder.php
+++ database/seeders/FourleafBillsElectricitySeeder.php
@@ -19,26 +19,49 @@ class FourleafBillsElectricitySeeder extends Seeder {
         'uid' => 202405,
         'kwh' => 222,
         'cost' => 2528.91,
+        'estimated_kwh' => false,
       ],
       [
         'uid' => 202406,
         'kwh' => 284,
         'cost' => 3435.15,
+        'estimated_kwh' => false,
       ],
       [
         'uid' => 202407,
         'kwh' => 224,
         'cost' => 2167.61,
+        'estimated_kwh' => false,
       ],
       [
         'uid' => 202408,
         'kwh' => 228,
         'cost' => 3017,
+        'estimated_kwh' => false,
       ],
       [
         'uid' => 202409,
         'kwh' => 300,
         'cost' => 3756.26,
+        'estimated_kwh' => false,
+      ],
+      [
+        'uid' => 202410,
+        'kwh' => 240,
+        'cost' => 3122.74,
+        'estimated_kwh' => true,
+      ],
+      [
+        'uid' => 202411,
+        'kwh' => 353,
+        'cost' => 3998.04,
+        'estimated_kwh' => true,
+      ],
+      [
+        'uid' => 202412,
+        'kwh' => 281,
+        'cost' => 3412.37,
+        'estimated_kwh' => false,
       ],
     ];
 
