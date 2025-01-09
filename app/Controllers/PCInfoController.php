d01fde409a922ecd72ee4768a8d59f6ce8906819
Rin Minase <rin.black.raison.detre@gmail.com>
2025-01-10 01:28:52 +0800

    ✨ feat(pc): added import to infos

--- app/Controllers/PCInfoController.php
+++ app/Controllers/PCInfoController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\JsonResponse;
 
 use App\Repositories\PCInfoRepository;
 use App\Repositories\PCSetupRepository;
+
 use App\Requests\PC\AddEditInfoRequest;
 use App\Requests\PC\AddEditSetupRequest;
+use App\Requests\ImportRequest;
+
 use App\Resources\DefaultResponse;
 use App\Resources\PC\PCInfoResource;
 use App\Resources\PC\PCInfoSummaryResource;
@@ -181,6 +184,58 @@ class PCInfoController extends Controller {
     $this->pcInfoRepository->delete($uuid);
 
     return DefaultResponse::success();
+  }
+
+  /**
+   * @OA\Post(
+   *   tags={"PC"},
+   *   path="/api/pc/infos/import",
+   *   summary="Import a JSON file to add (does not delete existing) data for PC infos table",
+   *   security={{"token":{}}},
+   *
+   *   @OA\RequestBody(
+   *     required=true,
+   *     @OA\MediaType(
+   *       mediaType="multipart/form-data",
+   *       @OA\Schema(
+   *         type="object",
+   *         @OA\Property(property="file", type="string", format="binary"),
+   *       ),
+   *     ),
+   *   ),
+   *
+   *   @OA\Response(
+   *     response=200,
+   *     description="Success",
+   *     @OA\JsonContent(
+   *       allOf={
+   *         @OA\Schema(ref="#/components/schemas/DefaultSuccess"),
+   *         @OA\Schema(
+   *           @OA\Property(property="data", ref="#/components/schemas/DefaultImportSchema"),
+   *         ),
+   *       },
+   *     ),
+   *   ),
+   *   @OA\Response(response=401, ref="#/components/responses/Unauthorized"),
+   *   @OA\Response(response=500, ref="#/components/responses/Failed"),
+   * )
+   */
+  public function import(ImportRequest $request): JsonResponse {
+    $file = $request->file('file')->get();
+
+    if (!is_json($file)) {
+      throw new JsonParsingException();
+    }
+
+    $data = json_decode($file);
+    $count = $this->pcInfoRepository->import($data);
+
+    return DefaultResponse::success(null, [
+      'data' => [
+        'acceptedImports' => $count,
+        'totalJsonEntries' => count($data),
+      ],
+    ]);
   }
 
   public function duplicate($uuid): JsonResponse {
--- app/Repositories/PCInfoRepository.php
+++ app/Repositories/PCInfoRepository.php
@@ -2,9 +2,11 @@
 
 namespace App\Repositories;
 
+use Carbon\Carbon;
 use Illuminate\Support\Str;
 
 use App\Models\PCInfo;
+use App\Models\PCOwner;
 
 class PCInfoRepository {
 
@@ -44,5 +46,41 @@ class PCInfoRepository {
     return PCInfo::where('uuid', $uuid)
       ->firstOrFail()
       ->delete();
+  }
+
+  public function import(array $contents) {
+    $import = [];
+
+    $owners = PCOwner::select('id', 'name')->get()->makeVisible('id');
+
+    foreach ($contents as $item) {
+      if (!empty($item)) {
+        if (!$item->id_owner) continue;
+        if (!$item->label) continue;
+
+        // Find the actual owner ID
+        $actual_owner = $owners->first(fn($owner) => $owner->name == $item->id_owner);
+        if (!$actual_owner) continue;
+
+        $data = [
+          'uuid' => Str::uuid()->toString(),
+          'id_owner' => $actual_owner->id,
+          'label' => $item->label,
+          'is_active' => to_boolean($item->is_active, true),
+          'is_hidden' => to_boolean($item->is_hidden, true),
+
+          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ];
+
+        array_push($import, $data);
+      }
+    }
+
+    PCInfo::refreshAutoIncrements();
+    PCInfo::insert($import);
+    PCInfo::refreshAutoIncrements();
+
+    return count($import);
   }
 }
