80561614f9500ca069047034d3721055758c94ef
Rin Minase <rin.black.raison.detre@gmail.com>
2025-01-12 22:42:13 +0800

    ✨ feat(pc): modified setup summary response

--- app/Resources/PC/PCSetupSummaryResource.php
+++ app/Resources/PC/PCSetupSummaryResource.php
@@ -12,6 +12,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
  *   @OA\Property(property="description", type="string", example="Component Description"),
  *   @OA\Property(property="count", type="integer", example=2),
  *   @OA\Property(property="isHidden", type="boolean", example=false),
+ *   @OA\Property(property="price", type="integer", example=1234),
+ *   @OA\Property(property="priceFormatted", type="string", example="1,234"),
+ *   @OA\Property(property="purhcaseDate", type="string", example="2020-10-10"),
+ *   @OA\Property(property="purhcaseLocation", type="string", example="Location"),
+ *   @OA\Property(property="purhcaseNotes", type="string", example="Notes"),
+ *   @OA\Property(property="isOnhand", type="boolean", example=true),
  * )
  */
 class PCSetupSummaryResource extends JsonResource {
@@ -24,6 +30,13 @@ class PCSetupSummaryResource extends JsonResource {
       'description' => $this->component->description ?? '',
       'count' => $this->count,
       'isHidden' => $this->is_hidden,
+
+      'price' => $this->component->price ?? null,
+      'priceFormatted' => $this->component->price ? number_format($this->component->price) : null,
+      'purchaseDate' => $this->component->purchase_date ?? '',
+      'purchaseLocation' => $this->component->purchase_location ?? '',
+      'purchaseNotes' => $this->component->purchase_notes ?? '',
+      'isOnhand' => $this->component->is_onhand ?? '',
     ];
   }
 }
