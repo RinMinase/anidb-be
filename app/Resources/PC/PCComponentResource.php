2cd8eb95ea9d74cf2c54bfc036ad99ac165fdd0c
Rin Minase <rin.black.raison.detre@gmail.com>
2025-01-28 02:20:35 +0800

    ✨ feat(pc): updated component api response

--- app/Resources/PC/PCComponentResource.php
+++ app/Resources/PC/PCComponentResource.php
@@ -4,15 +4,36 @@ namespace App\Resources\PC;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *   @OA\Property(property="id", type="integer", format="int32", example=1),
+ *   @OA\Property(property="idType", type="integer", format="int32", example=1),
+ *   @OA\Property(property="name", type="string", example="Sample Component Name"),
+ *   @OA\Property(property="description", type="string", example="Sample Component Description"),
+ *   @OA\Property(property="price", type="integer", format="int32", example=10000),
+ *   @OA\Property(property="priceEstimate", type="integer", format="int32", example=15000),
+ *   @OA\Property(property="purchaseDate", type="string", example="2020-10-01"),
+ *   @OA\Property(property="purchaseLocation", type="string", example="Store Name"),
+ *   @OA\Property(property="purchaseNotes", type="string", example="Some notes"),
+ *   @OA\Property(property="isOnhand", type="boolean", example=true),
+ *   @OA\Property(property="isPurchased", type="boolean", example=true),
+ *   @OA\Property(property="descriptiveName", type="string", example="Sample Component Name (10,000)", description="{name} ({price})"),
+ * ),
+ */
 class PCComponentResource extends JsonResource {
 
   public function toArray($request) {
     return [
       'id' => $this->id,
+      'idType' => $this->type->id,
       'type' => $this->type,
 
       'name' => $this->name,
       'description' => $this->description,
+
+      // On collection only
+      // If names contains duplicate, this shows {name} ({price})
+      'descriptiveName' => $this->name . ' (' . number_format($this->price ?? 0) . ')',
 
       'price' => $this->price,
       'purchaseDate' => $this->purchase_date,
