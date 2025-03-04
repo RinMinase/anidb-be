3bd1835f4c09adf3891cfa8a95b99e9caaeaf08d
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-05 04:51:13 +0800

    ✨ feat: removed usage of other db types

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
   /*
@@ -15,7 +13,7 @@ return [
     |
     */
 
-  'default' => env('DB_CONNECTION', 'pgsql'),
+  'default' => 'pgsql',
 
   /*
     |--------------------------------------------------------------------------
@@ -34,35 +32,6 @@ return [
     */
 
   'connections' => [
-
-    'sqlite' => [
-      'driver' => 'sqlite',
-      'url' => env('DATABASE_URL'),
-      'database' => env('DB_DATABASE', database_path('database.sqlite')),
-      'prefix' => '',
-      'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-    ],
-
-    'mysql' => [
-      'driver' => 'mysql',
-      'url' => env('DATABASE_URL'),
-      'host' => env('DB_HOST', '127.0.0.1'),
-      'port' => env('DB_PORT', '3306'),
-      'database' => env('DB_DATABASE', 'forge'),
-      'username' => env('DB_USERNAME', 'forge'),
-      'password' => env('DB_PASSWORD', ''),
-      'unix_socket' => env('DB_SOCKET', ''),
-      'charset' => 'utf8mb4',
-      'collation' => 'utf8mb4_unicode_ci',
-      'prefix' => '',
-      'prefix_indexes' => true,
-      'strict' => true,
-      'engine' => null,
-      'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-      ]) : [],
-    ],
-
     'pgsql' => [
       'driver' => 'pgsql',
       'url' => env('DATABASE_URL'),
