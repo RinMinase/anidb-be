ac143cbedcb69f1f797e2cbe4976072fc96b214c
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-05 08:42:02 +0800

    ✨ feat: added migration to remove rss tables

---
+++ database/migrations/2025_03_05_003751_drop_rss_tables.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::dropIfExists('rss_items');
+    Schema::dropIfExists('rss');
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+  }
+};
