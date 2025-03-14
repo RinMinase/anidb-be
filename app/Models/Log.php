8098df259af8ecec75453f26a1a1ea30ea19ec93
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-15 02:12:39 +0800

    📝 docs(log): updated api docs

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Traits\RefreshableAutoIncrements;
 
+/**
+ * @OA\Schema(
+ *   @OA\Property(
+ *     property="uuid",
+ *     type="string",
+ *     format="uuid",
+ *     example="9ef81943-78f0-4d1c-a831-a59fb5af339c",
+ *   ),
+ *   @OA\Property(property="tableChanged", type="string", example="marathon"),
+ *   @OA\Property(property="idChanged", type="string", example=1),
+ *   @OA\Property(
+ *     property="description",
+ *     type="string",
+ *     example="title changed from 'old' to 'new'",
+ *   ),
+ *   @OA\Property(property="action", type="string", example="add"),
+ *   @OA\Property(
+ *     property="createdAt",
+ *     type="string",
+ *     format="date-time",
+ *     example="2020-01-01 00:00:00",
+ *   ),
+ * )
+ */
 class Log extends Model {
 
   use RefreshableAutoIncrements;
