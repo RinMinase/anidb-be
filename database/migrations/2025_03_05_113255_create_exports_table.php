6e375eef95b9082a41558864402007d20cddf2eb
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-15 02:13:21 +0800

    ✨ feat(exports): added exports table

---
+++ database/migrations/2025_03_05_113255_create_exports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('exports', function (Blueprint $table) {
+      $table->uuid('id')->default(DB::raw('(gen_random_uuid())'))->primary();
+
+      $table->enum('type', ['json', 'sql', 'xlsx'])->default('json');
+      $table->timestamp('expires');
+      $table->boolean('is_finished');
+      $table->boolean('is_automated');
+
+      $table->timestamp('created_at');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+    Schema::dropIfExists('exports');
+  }
+};
