4a5031ff94e256bf0bf612415b16074ec29e93de
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-16 05:55:11 +0800

    ♻️ refactor: moved console commands location

--- app/Commands/GenerateRootPassword.php
+++ app/Commands/GenerateRootPassword.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
--- app/Commands/PruneOldLogData.php
+++ app/Commands/PruneOldLogData.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Commands;
 
 use App\Repositories\LogRepository;
 use Illuminate\Console\Command;
