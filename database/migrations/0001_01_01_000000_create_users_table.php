adcceb25fb3139b8f863a57de51472f0b0dfdfae
Rin Minase <rin.black.raison.detre@gmail.com>
2025-04-09 03:28:59 +0800

    ✨ feat: added session table, renamed migration scripts, consolidated users migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration {
 
@@ -16,9 +16,30 @@ class CreateUsersTable extends Migration {
 
       $table->timestamps();
     });
+
+    Schema::create('password_reset_tokens', function (Blueprint $table) {
+      $table->unlogged();
+
+      $table->string('email')->primary();
+      $table->string('token');
+      $table->timestamp('created_at')->nullable();
+
+      $table->unlogged(false);
+    });
+
+    Schema::create('sessions', function (Blueprint $table) {
+      $table->string('id')->primary();
+      $table->foreignId('user_id')->nullable()->index();
+      $table->string('ip_address', 45)->nullable();
+      $table->text('user_agent')->nullable();
+      $table->longText('payload');
+      $table->integer('last_activity')->index();
+    });
   }
 
   public function down() {
     Schema::dropIfExists('users');
+    Schema::dropIfExists('password_reset_tokens');
+    Schema::dropIfExists('sessions');
   }
 }
