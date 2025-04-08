0ad247578a9fde715dbe60275e90fe8aee359661
Rin Minase <rin.black.raison.detre@gmail.com>
2025-04-09 03:39:11 +0800

    ✨ feat: reduced view config file

--- config/view.php
+++ config/view.php
@@ -1,18 +1,9 @@
 <?php
 
 return [
-
   /* View / Blade Storage Paths */
-
-  'paths' => [
-    realpath(base_path('app')),
-  ],
+  'paths' => [ realpath(base_path('app')) ],
 
   /* Compiled View Path */
-
-  'compiled' => env(
-    'VIEW_COMPILED_PATH',
-    realpath(storage_path('framework/views'))
-  ),
-
+  'compiled' => realpath(storage_path('framework/views')),
 ];
