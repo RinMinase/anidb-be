5792c81c0475f43014fd77caa5588c281f69a83a
Rin Minase <rin.black.raison.detre@gmail.com>
2025-04-09 03:50:07 +0800

    ✨ feat: removed usages of maintenance mode

--- public/index.php
+++ public/index.php
@@ -4,11 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode
-// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-//     require $maintenance;
-// }
-
 // Register the Composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
