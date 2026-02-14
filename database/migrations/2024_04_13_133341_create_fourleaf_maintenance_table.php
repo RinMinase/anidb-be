2a102939ea19174a3decbe81cce4d5edd5fc2e19
Rin Minase <rin.black.raison.detre@gmail.com>
2026-02-14 14:41:29 +0800

    🐛 fix(fourleaf_gas): modified odo column to be nullable

--- database/migrations/2024_04_13_133341_create_fourleaf_maintenance_table.php
+++ database/migrations/2024_04_13_133341_create_fourleaf_maintenance_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
 
       $table->date('date');
       $table->string('description');
-      $table->mediumInteger('odometer');
+      $table->mediumInteger('odometer')->nullable();
     });
   }
 
