942c15ee6c8e851d29c207d4e3aef5bb51c573c4
Rin Minase <rin.black.raison.detre@gmail.com>
2026-03-16 00:38:08 +0800

    📝 docs(electricity): updated api docs

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -49,6 +49,7 @@ use OpenApi\Attributes as OA;
 #[OA\Tag(name: "PC")]
 #[OA\Tag(name: "Car")]
 #[OA\Tag(name: "Recipes")]
+#[OA\Tag(name: "Electricity")]
 #[OA\Tag(name: "Fourleaf - Electricity")]
 #[OA\Tag(name: "Fourleaf - Bills")]
 
