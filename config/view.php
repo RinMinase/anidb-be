f436a4859963c4c65b835038627639fed0c06551
Rin Minase <rin.black.raison.detre@gmail.com>
2026-03-24 16:21:37 +0800

    ✨ feat: organized folder structure for views

--- config/view.php
+++ config/view.php
@@ -2,7 +2,7 @@
 
 return [
   /* View / Blade Storage Paths */
-  'paths' => [ realpath(base_path('app')) ],
+  'paths' => [realpath(base_path('app/Views'))],
 
   /* Compiled View Path */
   'compiled' => realpath(storage_path('framework/views')),
